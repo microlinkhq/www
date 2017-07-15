5381f0436fbd2af327e4c044633d446a255c6d5d
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-07-15 14:25:17 +0200

    Update config

--- components/layout.js
+++ components/layout.js
@@ -10,34 +10,36 @@ export default ({
   twitter = APP_TWITTER,
   url = APP_URL,
   className
-}) => (
-  <div>
-    <Head>
-      <title>{ title }</title>
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>{ title }</title>
 
-      <meta itemProp='name' content={title} />
-      <meta itemProp='description' content={description} />
-      <meta itemProp='image' content={image} />
+        <meta itemProp='name' content={title} />
+        <meta itemProp='description' content={description} />
+        <meta itemProp='image' content={image} />
 
-      <meta name='twitter:card' content='summary_large_image' />
-      <meta name='twitter:title' content={title} />
-      <meta name='twitter:description' content={description} />
-      <meta name='twitter:site' content={twitter} />
-      <meta name='twitter:image' content={image} />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content={title} />
+        <meta name='twitter:description' content={description} />
+        <meta name='twitter:site' content={twitter} />
+        <meta name='twitter:image' content={image} />
 
-      <meta name='og:title' content={title} />
-      <meta name='og:description' content={description} />
-      <meta name='og:image' content={image} />
-      <meta name='og:url' content={url} />
-      <meta name='og:site_name' content={title} />
-      <meta name='og:type' content='website' />
+        <meta name='og:title' content={title} />
+        <meta name='og:description' content={description} />
+        <meta name='og:image' content={image} />
+        <meta name='og:url' content={url} />
+        <meta name='og:site_name' content={title} />
+        <meta name='og:type' content='website' />
 
-      <link rel='canonical' href={url} />
+        <link rel='canonical' href={url} />
 
-    </Head>
+      </Head>
 
-    <main className={className}>
-      { children }
-    </main>
-  </div>
-)
+      <main className={className}>
+        { children }
+      </main>
+    </div>
+  )
+}
