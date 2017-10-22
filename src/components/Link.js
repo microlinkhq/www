f2ec52d672db5028e8ebffecbb424c1fcf99db1a
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-10-22 18:16:10 +0200

    Improve hover effect

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,11 +1,37 @@
 import styled from 'styled-components'
 import {colors} from '../theme'
 
+const BORDER_WIDTH = '2px'
+
 export default styled.a`
-  display: inline-block;
+  display: inline;
   text-decoration: none;
   font-weight: 600;
-  border-bottom: 2px solid ${colors.gray2};
+  border-bottom: ${BORDER_WIDTH} solid ${colors.gray2};
   color: ${colors.gray8};
   line-height: normal;
+  position: relative;
+
+  &::before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: ${BORDER_WIDTH};
+    bottom: -${BORDER_WIDTH};
+    left: 0;
+    background-color: ${colors.gray9};
+    visibility: hidden;
+    transform: scaleX(0);
+    transition: all 0.10s ease-in-out 0s;
+  }
+
+  &:hover:before {
+    visibility: visible;
+    transform: scaleX(1);
+  }
+
+  &:active,
+  &:hover {
+    outline-width: 0;
+  }
 `
