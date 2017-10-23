a5001ab195343368f69c002fa0a6b5bbf4f53fc9
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-10-24 01:05:52 +0200

    Fix problem with xor module

---
+++ gatsby-node.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const webpack = require('webpack')
+
+exports.modifyWebpackConfig = ({ config, stage }) => {
+  // See https://github.com/FormidableLabs/react-live/issues/5
+  config.plugin('ignore', () => new webpack.IgnorePlugin(/^(xor|props)$/))
+
+  return config
+}
