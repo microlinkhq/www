fb3a5902a4b5666948fc58af4389294a339cd2d8
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-02-18 12:00:24 +0100

    Update Pricing Table Resume Better tooltip component

--- src/components/PricingTable.js
+++ src/components/PricingTable.js
@@ -1,13 +1,13 @@
-import styled, { keyframes } from 'styled-components'
-import { HelpCircle } from 'react-feather'
-import { Flex, Text, Lead } from 'rebass'
-import React, { Component } from 'react'
+import styled, {keyframes} from 'styled-components'
+import {HelpCircle, Check} from 'react-feather'
+import {Flex, Text, Lead} from 'rebass'
+import React, {Component} from 'react'
 
 import PricePicker from './PricePicker'
 import Checkout from './Checkout'
-import { LinkSolid } from './Link'
+import {LinkSolid} from './Link'
 import Tooltip from './ToolTip'
-import { colors } from '../theme'
+import {colors} from '../theme'
 
 const BASE_PRICE = 9
 const BASE_PLAN = 'pro-1k'
@@ -46,7 +46,7 @@ const TdPrice = Td.extend`
   font-weight: bold;
 
   &::before {
-    content: "$";
+    content: '$';
     font-weight: 100;
     font-size: 0.8em;
     position: relative;
@@ -56,7 +56,7 @@ const TdPrice = Td.extend`
   }
 
   &::after {
-    content: "/month";
+    content: '/month';
     font-weight: 100;
     font-size: 0.8em;
     position: relative;
@@ -69,7 +69,7 @@ const SubSpan = styled.span`
   &::after {
     font-weight: 100;
     font-size: 0.8em;
-    content: "/month";
+    content: '/month';
     position: relative;
     top: 0;
     color: ${colors.black50};
@@ -78,13 +78,13 @@ const SubSpan = styled.span`
 
 const DailyRequests = SubSpan.extend`
   &::after {
-    content: "/day";
+    content: '/day';
   }
 `
 
 const RequestsConcurrency = SubSpan.extend`
   &::after {
-    content: "/seq";
+    content: '/seq';
   }
 `
 
@@ -122,7 +122,7 @@ export default class extends Component {
     }
   }
 
-  priceSelected ({ plan, reqs }) {
+  priceSelected ({plan, reqs}) {
     const newPrice = BASE_PRICE * reqs / BASE_PLAN_REQS
 
     this.setState({
@@ -135,20 +135,20 @@ export default class extends Component {
     if (this.raf) return
     if (this.state.highlight) {
       // reset the animation
-      this.setState({ highlight: false }, () => {
+      this.setState({highlight: false}, () => {
         this.raf = requestAnimationFrame(() => {
           this.raf = null
-          this.setState({ highlight: true })
+          this.setState({highlight: true})
         })
       })
     } else {
-      this.setState({ highlight: true })
+      this.setState({highlight: true})
     }
   }
 
   render () {
-    const { highlight, plan, description, panelLabel, price } = this.state
-    const { api, apiKey, stripeKey } = this.props
+    const {highlight, plan, description, panelLabel, price} = this.state
+    const {api, apiKey, stripeKey} = this.props
 
     return (
       <div>
@@ -167,12 +167,11 @@ export default class extends Component {
               <Th>
                 <Tooltip
                   content={
-                    <div key='rate-limits'>
+                    <div>
                       <Text>Rate limit is based in a daily quota of requests.</Text>
                       <Text>It will be reset every day</Text>
                     </div>
-                  }
-                >
+                  }>
                   <Flex justify='center' align='center'>
                     <Text bold pr={1}>
                       Rate Limit
@@ -190,7 +189,8 @@ export default class extends Component {
                     plan: BASE_PLAN,
                     reqs: BASE_PLAN_REQS
                   }}
-                  onChange={this.priceSelected} />
+                  onChange={this.priceSelected}
+                />
                 <DailyRequests>reqs</DailyRequests>
               </Td>
             </Tr>
@@ -198,31 +198,10 @@ export default class extends Component {
               <Th>
                 <Tooltip
                   content={
-                    <div key='request-caching'>
-                      <Text>We follow a query caching policy for successive API calls.</Text>
-                    </div>
-                  }
-                >
-                  <Flex justify='center' align='center'>
-                    <Text bold pr={1}>
-                      Request Caching
-                    </Text>
-                    <HelpCircle color={colors.black50} size={14} />
-                  </Flex>
-                </Tooltip>
-              </Th>
-              <Td>5 days</Td>
-              <Td>Custom</Td>
-            </Tr>
-            <Tr>
-              <Th>
-                <Tooltip
-                  content={
-                    <div key='request-concurrency'>
+                    <div>
                       <Text>Maximum simultaneous requests you can make.</Text>
                     </div>
-                  }
-                >
+                  }>
                   <Flex justify='center' align='center'>
                     <Text bold pr={1}>
                       Request Concurrency
@@ -236,6 +215,52 @@ export default class extends Component {
               </Td>
               <Td>
                 ∞<RequestsConcurrency />
+              </Td>
+            </Tr>
+            <Tr>
+              <Th>
+                <Tooltip
+                  content={
+                    <div>
+                      <Text>We follow a query caching policy for successive API calls.</Text>
+                    </div>
+                  }>
+                  <Flex justify='center' align='center'>
+                    <Text bold pr={1}>
+                      Request Caching
+                    </Text>
+                    <HelpCircle color={colors.black50} size={14} />
+                  </Flex>
+                </Tooltip>
+              </Th>
+              <Td>
+                <Check color={colors.black} size={16} />
+              </Td>
+              <Td>
+                <Check color={colors.black} size={16} />
+              </Td>
+            </Tr>
+            <Tr>
+              <Th>
+                <Tooltip
+                  content={
+                    <div>
+                      <Text>We provide chat support to help you integrate with your services.</Text>
+                    </div>
+                  }>
+                  <Flex justify='center' align='center'>
+                    <Text bold pr={1}>
+                      Live Support
+                    </Text>
+                    <HelpCircle color={colors.black50} size={14} />
+                  </Flex>
+                </Tooltip>
+              </Th>
+              <Td>
+                <Check color={colors.black} size={16} />
+              </Td>
+              <Td>
+                <Check color={colors.black} size={16} />
               </Td>
             </Tr>
             <Tr>
@@ -261,10 +286,10 @@ export default class extends Component {
         </Table>
 
         <Flex is='section' justify='center' direction='column' align='center'>
-          <Text pt={4} px={5} f={3} color='gray8' style={{ textAlign: 'center' }}>
+          <Text pt={4} px={5} f={3} color='gray8' style={{textAlign: 'center'}}>
             Do you need more?
           </Text>
-          <Text pt={3} px={5} f={3} color='gray8' style={{ textAlign: 'center' }}>
+          <Text pt={3} px={5} f={3} color='gray8' style={{textAlign: 'center'}}>
             <LinkSolid onClick={this.openForm}>Contact us</LinkSolid>.
           </Text>
         </Flex>
--- src/components/ToolTip.js
+++ src/components/ToolTip.js
@@ -1,49 +1,28 @@
 import React from 'react'
-import {Tooltip} from 'react-lightweight-tooltip'
-import styled, {keyframes} from 'styled-components'
+import ReactARIAToolTip from 'react-aria-tooltip'
+import {keyframes} from 'styled-components'
 
-const tooltipAnimation = keyframes`
+const fadeIn = keyframes`
   0% {
     opacity: 0;
   }
-
   to {
     opacity: 1;
   }
 `
 
-const TooltipWrapper = styled.div`
-  > div {
-    line-height: normal;
-    font-weight: inherit;
+const Tooltip = ReactARIAToolTip.extend`
+cursor: help;
 
-    > div {
-      animation ${tooltipAnimation} .35s forwards cubic-bezier(.215,.61,.355,1);
-      font-weight: normal;
-    }
-  }
+.ra-tooltip {
+  animation ${fadeIn} .35s forwards cubic-bezier(.215,.61,.355,1);
+}
 `
 
-const STYLES = {
-  wrapper: {
-    color: 'inherit'
-  },
-  tooltip: {
-    width: '300px',
-    padding: '0 8px',
-    lineHeight: '1.6',
-    borderRadius: '2px',
-    letterSpacing: 'normal'
-    // fontSize: '.85rem'
-  }
-}
-
 export default ({content, children, ...props}) => (
-  <TooltipWrapper>
-    <Tooltip
-      styles={STYLES}
-      content={[content]}>
-      {children}
-    </Tooltip>
-  </TooltipWrapper>
+  <Tooltip
+    eventType='hover'
+    message={[content]}>
+    {children}
+  </Tooltip>
 )
