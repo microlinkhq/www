2d70120b8bf5a7a59e154d3a9eede34d8c3d7034
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-02-25 22:58:56 +0100

    Update build process

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,15 +5,5 @@ const webpack = require('webpack')
 exports.modifyWebpackConfig = ({ config, stage }) => {
   // See https://github.com/FormidableLabs/react-live/issues/5
   config.plugin('ignore', () => new webpack.IgnorePlugin(/^(xor|props)$/))
-
-  exports.modifyWebpackConfig = ({ config, stage }) => {
-    if (stage === 'build-html') {
-      config.loader('null', {
-        test: /react-microlink/,
-        loader: 'null-loader'
-      })
-    }
-  }
-
   return config
 }
