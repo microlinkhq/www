48bcb4319c9b8e03a6a7512bc677ab6e42e4a35f
Brad Adams <hi@breadadams.com>
2018-04-04 00:53:46 +0200

    Update `ContentFeature` pattern component

--- src/components/patterns/ContentFeature.js
+++ src/components/patterns/ContentFeature.js
@@ -15,7 +15,7 @@ CustomImage.defaultProps = {
 
 export default ({ image, children, title, flexDirection = 'left' }) => (
   <Flex
-    alignContent={'center'}
+    align='center'
     flexDirection={[
       'column',
       '',
