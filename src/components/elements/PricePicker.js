1ba52d4507aafedfe2dd843fb7ab3bdab3db625c
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-08-21 13:09:48 +0200

    Update plans

--- src/components/elements/PricePicker.js
+++ src/components/elements/PricePicker.js
@@ -3,19 +3,19 @@ import { Box, Select, Label } from 'components/elements'
 
 export const DEFAULT_PLAN = {
   reqs: 1000,
-  planId: 'pro-1k'
+  planId: 'pro-1k-v2'
 }
 
-export const BASE_PLAN_PRICE = 9
+export const BASE_PLAN_PRICE = 12
 
 export const PLANS = {
-  1000: { planId: 'pro-1k', text: '1K' },
-  3000: { planId: 'pro-3k', text: '3K' },
-  10000: { planId: 'pro-10k', text: '10K' },
-  50000: { planId: 'pro-50k', text: '50K' },
-  100000: { planId: 'pro-100k', text: '100K' },
-  500000: { planId: 'pro-500k', text: '500K' },
-  1000000: { planId: 'pro-1m', text: '1M' }
+  1000: { planId: 'pro-1k-v2', text: '1K' },
+  3000: { planId: 'pro-3k-v2', text: '3K' },
+  10000: { planId: 'pro-10k-v2', text: '10K' },
+  50000: { planId: 'pro-50k-v2', text: '50K' },
+  100000: { planId: 'pro-100k-v2', text: '100K' },
+  500000: { planId: 'pro-500k-v2', text: '500K' },
+  1000000: { planId: 'pro-1m-v2', text: '1M' }
 }
 
 export default class extends Component {
