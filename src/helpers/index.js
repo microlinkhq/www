746b84e00dc0342798ff75ef7ac2feabcf723171
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-11-12 20:05:00 +0100

    PricePicker refactor

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,8 @@
+import formatNumber from './format-number'
 import formatDate from './format-date'
 import unmarshall from './unmarshall'
 import marshall from './marshall'
 import get from './get'
 import createApiUrl from './create-api-url'
 
-export { marshall, unmarshall, get, formatDate, createApiUrl }
+export { marshall, unmarshall, get, formatDate, formatNumber, createApiUrl }
