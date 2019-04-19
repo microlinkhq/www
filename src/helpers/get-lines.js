f632198012a3006ad0849bfb46a9e20fd1810b72
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-04-19 21:30:12 +0200

    build: add testing using ava

---
+++ src/helpers/get-lines.js
@@ -0,0 +1,18 @@
+const REGEX_ONE_LINE = /\{\d\d?\}/
+const REGEX_LINE_RANGE = /\{\d\d?,\d\d?\}/
+
+export default str => {
+  if (!str) return null
+
+  let strMatch
+  ;[REGEX_ONE_LINE, REGEX_LINE_RANGE].find(
+    regex => (strMatch = str.match(regex))
+  )
+  if (!strMatch) return null
+
+  return strMatch[0]
+    .replace('{', '')
+    .replace('}', '')
+    .split(',')
+    .map(n => Number(n.trim()))
+}
