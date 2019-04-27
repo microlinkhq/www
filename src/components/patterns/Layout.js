80b56ccddd17494368021649f624984c547b679f
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-04-27 16:06:50 +0200

    build: smooth scroll for any link

--- src/components/patterns/Layout.js
+++ src/components/patterns/Layout.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { useEffect, Fragment } from 'react'
 import { ThemeProvider } from 'styled-components'
 
 import Head from 'components/Head'
@@ -9,21 +9,37 @@ import { Toolbar, Footer, CookiesPolicy } from 'components/patterns'
 import theme from 'theme'
 import 'styles/main.scss'
 
-const Layout = ({ footer, children }) => (
-  <ThemeProvider theme={theme}>
-    <Fragment>
-      <Head />
-      <Toolbar />
-      <Box pt={TOOLBAR_HEIGHT}>{children}</Box>
-      <CookiesPolicy />
-      {footer && <Footer />}
-      <script
-        crossOrigin='anonymous'
-        src='https://polyfill.io/v3/polyfill.min.js?features=fetch-polyfill'
-      />
-    </Fragment>
-  </ThemeProvider>
-)
+if (global.window) {
+  window.scroll = require('smooth-scroll')('a[href*="#"]')
+}
+
+const Layout = ({ footer, children }) => {
+  useEffect(() => {
+    const { hash } = window.location
+    if (hash) {
+      const node = document.querySelector(hash)
+      if (node) {
+        setTimeout(() => window.scroll.animateScroll(node), 450)
+      }
+    }
+  }, [])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Fragment>
+        <Head />
+        <Toolbar />
+        <Box pt={TOOLBAR_HEIGHT}>{children}</Box>
+        <CookiesPolicy />
+        {footer && <Footer />}
+        <script
+          crossOrigin='anonymous'
+          src='https://polyfill.io/v3/polyfill.min.js?features=fetch-polyfill'
+        />
+      </Fragment>
+    </ThemeProvider>
+  )
+}
 
 Layout.defaultProps = {
   footer: true
