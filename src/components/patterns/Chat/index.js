446823408c9175afdd1497bbbc45fdf6ac4d5dd1
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-04-28 13:28:19 +0200

    build: improve chat page

--- src/components/patterns/Chat/index.js
+++ src/components/patterns/Chat/index.js
@@ -1,16 +1,25 @@
 import React from 'react'
 
-export default () => (
-  <div style={{ display: 'flex', justifyContent: 'center', margin: '0 auto' }}>
-    <iframe
-      title='chat'
-      frameBorder='0'
-      target='_parent'
-      src='https://chat.microlink.io/iframe/dialog'
-      style={{
-        width: '250px',
-        height: '250px'
-      }}
-    />
-  </div>
-)
+import { Flex } from 'components/elements'
+
+export default ({ large }) => {
+  const iframeUrl = large
+    ? `https://chat.microlink.io/iframe/dialog?large=true`
+    : `https://chat.microlink.io/iframe/dialog`
+
+  const style = large
+    ? { width: '350px', height: '350px' }
+    : { width: '250px', height: '250px' }
+
+  return (
+    <Flex justifyContent='center' mx='auto'>
+      <iframe
+        title='chat'
+        frameBorder='0'
+        target='_parent'
+        src={iframeUrl}
+        style={style}
+      />
+    </Flex>
+  )
+}
--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
 import { Layout, Chat } from 'components/patterns'
+import { Container, Subhead, Box, Flex } from 'components/elements'
+import { Plus } from 'react-feather'
+import { Microlink, Slack } from 'components/logos'
+
+const LOGO_SIZE = 60
+const PLUS_SIZE = LOGO_SIZE * 0.35
 
 export default () => (
   <Layout title='Chat'>
-    <Chat />
+    <Container width='100vh'>
+      <Subhead py={5}>Come Chat with Us</Subhead>
+      <Flex alignItems='center' justifyContent='center'>
+        <Microlink size={LOGO_SIZE} />
+        <Box color='gray' px={3}>
+          <Plus size={PLUS_SIZE} />
+        </Box>
+        <Slack size={LOGO_SIZE} />
+      </Flex>
+      <Flex alignItems='center' justifyContent='center' py={5}>
+        <Chat large />
+      </Flex>
+    </Container>
   </Layout>
 )
