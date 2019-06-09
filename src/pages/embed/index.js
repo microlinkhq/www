12ed02c22cfdc8b53471b0df5e1ea2069a2bb66c
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-06-09 20:57:52 +0200

    refactor: unify shared components

---
+++ src/pages/embed/index.js
@@ -0,0 +1,56 @@
+import React, { useState, useRef } from 'react'
+import { useDemoLinks } from 'components/hook'
+import { Layout } from 'components/patterns'
+import { isEmpty } from 'lodash'
+import { unmarshall } from 'helpers'
+import mql from '@microlink/mql'
+
+import Examples from './examples'
+import Template from './template'
+
+export default () => {
+  const inputEl = useRef(null)
+  const demoLinks = useDemoLinks()
+  const [isLoading, setLoading] = useState(false)
+  const [data, setData] = useState(null)
+  const [url, setUrl] = useState('')
+
+  React.useEffect(() => {
+    const { url: targetUrl } = unmarshall(window.location.search)
+    if (targetUrl) setUrl(targetUrl)
+  }, [])
+
+  React.useEffect(() => {
+    async function fetchData () {
+      try {
+        const { data } = await mql(url)
+        setData(data)
+      } catch (err) {}
+    }
+    fetchData()
+    setLoading(false)
+  }, [url])
+
+  const onSubmit = async event => {
+    setLoading(true)
+    event.preventDefault()
+    const url = inputEl.current.value
+    setUrl(url)
+  }
+
+  return (
+    <Layout>
+      {isEmpty(data) ? (
+        <Examples
+          demoLinks={demoLinks}
+          onSubmit={onSubmit}
+          url={url}
+          innerRef={inputEl}
+          isLoading={isLoading}
+        />
+      ) : (
+        <Template data={data} />
+      )}
+    </Layout>
+  )
+}
