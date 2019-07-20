b6d88f2a1254a4b9d2268321e8330db13359cc4d
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-07-20 12:56:28 +0200

    fix: pass onLoad event

--- src/components/elements/Image/LazyImage.js
+++ src/components/elements/Image/LazyImage.js
@@ -1,20 +1,28 @@
 import React, { useState, createElement } from 'react'
+import { noop, isNil } from 'lodash'
+
 import ImagePlaceholder from './ImagePlaceholder'
 import Image from './Image'
-import { isNil } from 'lodash'
 
 const LazyImage = ({ lazy, loading, ...props }) => {
   const [isLoaded, setLoaded] = useState(false)
 
   if (!isNil(loading) ? !loading : isLoaded) return createElement(Image, props)
 
-  const onLoad = () => setLoaded(true)
+  const onLoad = event => {
+    setLoaded(true)
+    props.onLoad(event)
+  }
 
   return (
     <ImagePlaceholder {...props}>
       <Image {...props} onLoad={onLoad} style={{ display: 'none' }} />
     </ImagePlaceholder>
   )
+}
+
+LazyImage.defaultProps = {
+  onLoad: noop
 }
 
 export default LazyImage
