fadb4b8ee0759f2d0429c2a15136217eb6fba952
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-09-06 19:41:09 +0200

    refactor: use hooks

--- src/components/elements/PricePicker.js
+++ src/components/elements/PricePicker.js
@@ -1,56 +1,52 @@
-import React, { Component } from 'react'
-import { Box, Select, Label } from 'components/elements'
-import humanNumber from 'human-number'
+import React, { useState } from 'react'
+import { Select, Label } from 'components/elements'
+import { formatNumber } from 'helpers'
+
+const BASE_PLAN_PRICE = 12
+
+const MONTH_DAYS = 28
 
 const createReqsLabels = reqsPerDay => ({
-  reqsPerDay,
-  humanReqsPerDay: humanNumber(reqsPerDay),
+  reqsPerMonth: formatNumber(reqsPerDay * MONTH_DAYS),
   monthlyPrice: calculateMonthlyPrice(reqsPerDay)
 })
 
 const calculateMonthlyPrice = reqsPerDay =>
   (reqsPerDay / 1000) * BASE_PLAN_PRICE
 
-export const BASE_PLAN_PRICE = 12
-
 export const PLANS = [
-  { planId: 'pro-1k-v2', ...createReqsLabels(1000) },
-  { planId: 'pro-2k-v2', ...createReqsLabels(2000) },
-  { planId: 'pro-3k-v2', ...createReqsLabels(3000) },
-  { planId: 'pro-10k-v2', ...createReqsLabels(10000) },
-  { planId: 'pro-50k-v2', ...createReqsLabels(50000) }
+  { planId: 'pro-1k-v2', ...createReqsLabels(1000), width: '4.5rem' },
+  { planId: 'pro-2k-v2', ...createReqsLabels(2000), width: '4.5rem' },
+  { planId: 'pro-3k-v2', ...createReqsLabels(3000), width: '4.5rem' },
+  { planId: 'pro-10k-v2', ...createReqsLabels(10000), width: '5rem' },
+  { planId: 'pro-50k-v2', ...createReqsLabels(50000), width: '5.7rem' }
 ]
 
-export const DEFAULT_PLAN = PLANS[1]
+export const DEFAULT_PLAN = PLANS[2]
 
-export default class extends Component {
-  state = { ...DEFAULT_PLAN }
+export default props => {
+  const [currentPlan, setCurrentPlan] = useState(DEFAULT_PLAN)
 
-  handleChange = event => {
-    event.preventDefault()
-    const { value: humanReqsPerDay } = event.target
-    const newState = PLANS.find(
-      plan => plan.humanReqsPerDay === humanReqsPerDay
-    )
-    this.setState(newState)
-    this.props.onChange(newState)
+  const handleChange = event => {
+    const reqsPerMonth = event.target.value
+    const newPlan = PLANS.find(plan => plan.reqsPerMonth === reqsPerMonth)
+    setCurrentPlan(newPlan)
+    props.onChange(newPlan)
   }
 
-  render () {
-    return (
-      <Box>
-        <Select
-          py='2px'
-          width={['3.2rem', '3.8rem']}
-          value={this.state.humanReqsPerDay}
-          onChange={this.handleChange}
-        >
-          {Object.keys(PLANS).map(plan => (
-            <option key={plan}>{PLANS[plan].humanReqsPerDay}</option>
-          ))}
-        </Select>
-        <Label ml={1} display='inline' children=' reqs' suffix='/day' />
-      </Box>
-    )
-  }
+  return (
+    <>
+      <Select
+        width={currentPlan.width}
+        value={currentPlan.reqsPerMonth}
+        onChange={handleChange}
+        selected={currentPlan.reqsPerMonth}
+      >
+        {Object.keys(PLANS).map(plan => (
+          <option key={plan}>{PLANS[plan].reqsPerMonth}</option>
+        ))}
+      </Select>
+      <Label ml={1} display='inline' children=' reqs' suffix='/month' />
+    </>
+  )
 }
