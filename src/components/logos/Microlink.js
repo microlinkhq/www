0aad13c8a89251fd0b95999fe42ae0417e71d690
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-11-03 10:27:29 +0100

    fix: load image

--- src/components/logos/Microlink.js
+++ src/components/logos/Microlink.js
@@ -2,5 +2,5 @@ import React from 'react'
 import { Image } from 'components/elements'
 
 export default props => (
-  <Image src='https://cdn.microlink.io/logo/logo.svg' {...props} />
+  <Image lazy={false} src='https://cdn.microlink.io/logo/logo.svg' {...props} />
 )
