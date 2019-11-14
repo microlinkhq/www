4f8c3933fb1f4265ebc5689a389e0b30a5421a66
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-11-14 15:24:42 +0100

    fix: typecheck url

--- src/components/patterns/Microlink/Microlink.js
+++ src/components/patterns/Microlink/Microlink.js
@@ -3,5 +3,6 @@ import Microlink from '@microlink/react'
 import { template } from 'helpers'
 
 export default ({ url, ...props }) => {
-  return <Microlink url={template(url)} style={{ margin: 'auto' }} {...props} />
+  if (url) url = template(url)
+  return <Microlink url={url} style={{ margin: 'auto' }} {...props} />
 }
