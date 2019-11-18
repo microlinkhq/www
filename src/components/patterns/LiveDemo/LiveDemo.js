298835d91c87c9ce8ea061b0747ab45bdf5568be
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-11-18 10:04:30 +0100

    refactor: use useState

--- src/components/patterns/LiveDemo/LiveDemo.js
+++ src/components/patterns/LiveDemo/LiveDemo.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
 import { Card, Flex } from 'components/elements'
 import Preview from './preview'
@@ -10,52 +10,44 @@ import {
   CARD_HEIGHT_MOBILE
 } from './theme'
 
-export default class extends Component {
-  state = { view: 'preview' }
+export default ({ children, loading }) => {
+  const [view, setView] = useState('preview')
+  const isSDK = view === 'preview'
 
-  render () {
-    const { view } = this.state
-    const { children, loading } = this.props
-    const isSDK = view === 'preview'
-
-    return (
-      <Flex flexDirection='column' justifyContent='space-around'>
-        <Flex flexDirection='column' mb={[4, 0]}>
-          <Card
-            css={`
-              transition: none;
-            `}
-            px={isSDK ? 0 : 3}
-            py={isSDK ? 0 : 3}
-            width={[CARD_WIDTH_MOBILE, CARD_WIDTH_DESKTOP]}
-            height={[CARD_HEIGHT_MOBILE, CARD_HEIGHT_DESKTOP]}
-            style={{
-              background: '#282a36',
-              padding: 0,
-              overflow: isSDK ? 'hidden' : 'auto'
-            }}
-          >
-            <Preview loading={loading} view={view} children={children} />
-          </Card>
-          <Flex justifyContent='flex-end'>
-            <Card.Option
-              children='preview'
-              value={view}
-              onClick={() => this.setState({ view: 'preview' })}
-            />
-            <Card.Option
-              children='json'
-              value={view}
-              onClick={() => this.setState({ view: 'json' })}
-            />
-            <Card.Option
-              children='code'
-              value={view}
-              onClick={() => this.setState({ view: 'code' })}
-            />
-          </Flex>
+  return (
+    <Flex flexDirection='column' justifyContent='space-around'>
+      <Flex flexDirection='column' mb={[4, 0]}>
+        <Card
+          px={isSDK ? 0 : 3}
+          py={isSDK ? 0 : 3}
+          width={[CARD_WIDTH_MOBILE, CARD_WIDTH_DESKTOP]}
+          height={[CARD_HEIGHT_MOBILE, CARD_HEIGHT_DESKTOP]}
+          style={{
+            background: '#282a36',
+            padding: 0,
+            overflow: isSDK ? 'hidden' : 'auto'
+          }}
+        >
+          <Preview loading={loading} view={view} children={children} />
+        </Card>
+        <Flex justifyContent='flex-end'>
+          <Card.Option
+            children='preview'
+            value={view}
+            onClick={() => setView('preview')}
+          />
+          <Card.Option
+            children='json'
+            value={view}
+            onClick={() => setView('json')}
+          />
+          <Card.Option
+            children='code'
+            value={view}
+            onClick={() => setView('code')}
+          />
         </Flex>
       </Flex>
-    )
-  }
+    </Flex>
+  )
 }
