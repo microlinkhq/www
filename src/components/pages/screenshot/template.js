f435b2bbd286f7a5b0ad476b2e54e540fce894fe
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-11-19 13:23:02 +0100

    fix: exclude titleize domain

--- src/components/pages/screenshot/template.js
+++ src/components/pages/screenshot/template.js
@@ -61,7 +61,12 @@ const Hero = ({ domain, brand, data }) => {
   const caption = (
     <>
       Turn{' '}
-      <Subhead as='span' color='black' fontWeight='regular'>
+      <Subhead
+        as='span'
+        color='black'
+        fontWeight='regular'
+        titleExclude={domain}
+      >
         {domain}
       </Subhead>{' '}
       links into a screenshot
