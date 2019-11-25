12bf647e0f533b052831272b78d2fcd2213e496d
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-11-25 13:49:56 +0100

    build: use react memo

--- src/components/elements/ClearbitLogo.js
+++ src/components/elements/ClearbitLogo.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { debounceComponent } from 'helpers'
 import { Image } from 'components/elements'
 
-const Clearbit = debounceComponent(Image)
-
-export default ({ companyName, ...props }) => (
-  <Clearbit src={`https://logo.clearbit.com/${companyName}`} {...props} />
+const Clearbit = ({ companyName, ...props }) => (
+  <Image src={`https://logo.clearbit.com/${companyName}`} {...props} />
 )
+
+export default React.memo(Clearbit)
