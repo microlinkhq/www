3fce0cdd5dc0d9813e5f7743d6671b26d5f21899
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-11-27 00:33:11 +0100

    refactor: better iframe defaults

--- src/components/elements/Iframe.js
+++ src/components/elements/Iframe.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import { aspectRatio } from 'helpers'
+
 import Flex from './Flex'
 
 export default props => (
-  <Flex as='iframe' width='100%' frameBorder='0' target='_parent' {...props} />
+  <Flex
+    as='iframe'
+    width={aspectRatio.widths}
+    height={aspectRatio.heights}
+    frameBorder='0'
+    target='_parent'
+    {...props}
+  />
 )
