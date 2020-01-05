6806cadd109be3fbeae4d7ec28d4b4830f810ec8
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-01-05 08:57:19 +0100

    build: add iframe lazy loader

--- src/components/elements/Iframe/Iframe.js
+++ src/components/elements/Iframe/Iframe.js
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 
 import Flex from '../Flex'
 import CodeEditor from '../CodeEditor/CodeEditor'
+import ImagePlaceholder from '../Image/ImagePlaceholder'
 
 export default ({
   width = CodeEditor.width,
   height = CodeEditor.height,
   ...props
-}) => (
-  <Flex
-    as='iframe'
-    mx='auto'
-    frameBorder='0'
-    target='_parent'
-    height={height}
-    width={width}
-    {...props}
-  />
-)
+}) => {
+  const inputEl = useRef(null)
+  const [isLoading, setLoading] = useState(true)
+  const setLoaded = () => setLoading(false)
+
+  useEffect(() => {
+    if (inputEl.current) {
+      const iframe = inputEl.current
+      if (iframe) {
+        iframe.addEventListener('load', setLoaded)
+        return () => iframe.removeEventListener('load', setLoaded)
+      }
+    }
+  }, [])
+
+  return (
+    <>
+      {isLoading ? <ImagePlaceholder width={width} height={height} /> : null}
+      <Flex
+        as='iframe'
+        ref={inputEl}
+        style={{ display: isLoading && 'none' }}
+        mx='auto'
+        frameBorder='0'
+        target='_parent'
+        height={height}
+        width={width}
+        {...props}
+      />
+    </>
+  )
+}
