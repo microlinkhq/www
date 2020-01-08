f0907b87f6e792ac8483afd03341b25236c6d6d1
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-01-08 19:59:14 +0000

    build: remove alt

--- src/components/elements/ClearbitLogo/ClearbitLogo.js
+++ src/components/elements/ClearbitLogo/ClearbitLogo.js
@@ -4,7 +4,6 @@ import { Image } from 'components/elements'
 const Clearbit = ({ companyName, ...props }) => (
   <Image
     lazy={false}
-    alt={`${companyName} logo`}
     src={`https://logo.clearbit.com/${companyName}`}
     {...props}
   />
