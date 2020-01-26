ba580323a0c04623b00ef0e8673898a8062432c2
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-01-26 14:00:01 +0100

    refactor: remove unused

--- src/components/patterns/Layout.js
+++ src/components/patterns/Layout.js
@@ -1,4 +1,4 @@
-import styled, { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 import React, { createElement, useEffect } from 'react'
 import { Location } from '@reach/router'
 
