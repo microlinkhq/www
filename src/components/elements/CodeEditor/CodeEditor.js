4642e2b533e1f5a3b509e2211519e6c84c57c810
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-02-15 21:55:09 +0100

    refactor: remove duplicate code

--- src/components/elements/CodeEditor/CodeEditor.js
+++ src/components/elements/CodeEditor/CodeEditor.js
@@ -1,16 +1,21 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { prettier, getLines, template, aspectRatio } from 'helpers'
-import { shadowOffsets, shadowColors, colors, fonts } from 'theme'
+import { prettier, getLines, template } from 'helpers'
 import styled, { css } from 'styled-components'
-import { Text, Box } from 'components/elements'
-import { rgba, opacity, lighten } from 'polished'
+import { Text } from 'components/elements'
+import { rgba, lighten } from 'polished'
 import React, { useState } from 'react'
 import identity from 'lodash/identity'
 import CodeCopy from 'react-codecopy'
+import { colors, fonts } from 'theme'
 import range from 'lodash/range'
 import get from 'dlv'
 
-const { width, height } = aspectRatio([0.3, 0.4, 0.6, 0.6])
+import {
+  styleTerminalHeader,
+  TerminalButton,
+  TerminalWindow,
+  TERMINAL_WIDTH
+} from '../Terminal/Terminal'
 
 const generateHighlighLines = linesRange => {
   if (!linesRange) return
@@ -30,6 +35,16 @@ const highlighLinesStyle = highlightLines => css`
     background: #464957;
   }
 `
+
+const getLanguage = (className, { language }) => {
+  if (language) return language
+  const languageFromClassName = className.split('-')[1]
+  if (languageFromClassName) return languageFromClassName.split('{')[0]
+  return 'javascript'
+}
+
+const getClassName = ({ className, metastring = '' }) =>
+  className ? className + metastring : ''
 
 export const COLORS = {
   PINK: colors.pink5,
@@ -159,30 +174,16 @@ const CustomSyntaxHighlighter = styled(SyntaxHighlighter)`
   }
 `
 
-const TerminalWindow = styled(Box)`
-  border-radius: 5px;
-  box-shadow: ${shadowOffsets[0]} ${shadowColors[0]};
-`
-
 const TerminalHeader = styled.header`
-  border-top-right-radius: 3px;
-  border-top-left-radius: 3px;
-  display: flex;
+  ${styleTerminalHeader};
   height: ${TERMINAL_HEADER_HEIGHT};
   background: ${COLORS.BACKGROUND};
-  align-items: center;
-  padding: 1rem;
-  position: sticky;
   top: 1px;
-  z-index: 0;
-`
+  z-index: 2;
 
-const TerminalButton = styled.div`
-  border-radius: 50px;
-  width: 12px;
-  height: 12px;
-  margin: 0 0.2rem;
-  background ${({ color }) => color};
+  .codecopy_wrapper {
+    top: 15px;
+  }
 `
 
 const TerminalTitleWrapper = styled('div')`
@@ -193,7 +194,7 @@ const TerminalTitleWrapper = styled('div')`
 `
 
 const TerminalText = styled.section`
-  overflow-x: auto;
+  overflow: visible;
   font-family: ${fonts.mono};
   font-size: 13px;
   font-weight: normal;
@@ -221,48 +222,40 @@ const TerminalText = styled.section`
 
 const TerminalTextWrapper = styled.div`
   word-break: break-all;
+  width: 100%;
 `
 
-const DefaultActionComponent = ({ toCopy, isHover, theme }) => (
-  <CodeCopy interactive={isHover} theme={theme} text={toCopy} />
-)
-
-function Terminal ({
-  title,
+const Terminal = ({
+  title = '',
   children,
   theme,
   interactive,
   toCopy,
-  ActionComponent,
+  ActionComponent = CodeCopy,
   ...props
-}) {
+}) => {
   const [isHover, setHover] = useState(interactive)
-  const onMouseOut = () => setHover(false)
-  const onMouseOver = () => setHover(true)
-  const ChildComponent = ActionComponent || DefaultActionComponent
 
   return (
     <TerminalWindow
-      onMouseOut={onMouseOut}
-      onMouseOver={onMouseOver}
+      onMouseOut={() => setHover(false)}
+      onMouseOver={() => setHover(true)}
       {...props}
     >
       <TerminalHeader>
-        <TerminalButton color='#FF5F56' />
-        <TerminalButton color='#FFBD2E' />
-        <TerminalButton color='#27C93F' />
-        {title && (
-          <TerminalTitleWrapper>
-            <Text
-              color={rgba(lighten(0.4, COLORS.BACKGROUND), 0.8)}
-              fontSize={0}
-              children={title}
-            />
-          </TerminalTitleWrapper>
-        )}
-        <ChildComponent isHover={isHover} theme={theme} toCopy={toCopy} />
+        <TerminalButton.Red theme={theme} />
+        <TerminalButton.Yellow theme={theme} />
+        <TerminalButton.Green theme={theme} />
+        <TerminalTitleWrapper>
+          <Text
+            color={rgba(lighten(0.4, COLORS.BACKGROUND), 0.8)}
+            fontSize={0}
+            children={title}
+          />
+        </TerminalTitleWrapper>
+        <ActionComponent isHover={isHover} theme={theme} toCopy={toCopy} />
       </TerminalHeader>
-      <TerminalText>{children}</TerminalText>
+      <TerminalText children={children} />
     </TerminalWindow>
   )
 }
@@ -278,24 +271,15 @@ const CodeEditor = props => {
     showLineNumbers,
     interactive,
     children,
+    theme,
     ...restProps
   } = props
 
-  const className = props.className
-    ? props.className + (props.metastring || '')
-    : ''
+  const className = getClassName(props)
   const highlightLines = getLines(className)
-
-  const language = (() => {
-    if (props.language) return props.language
-    const languageFromClassName = className.split('-')[1]
-    if (languageFromClassName) return languageFromClassName.split('{')[0]
-    return 'javascript'
-  })()
-
+  const language = getLanguage(className, props)
   const pretty = props.prettier ? get(prettier, language, identity) : identity
   const text = pretty(template(children)).trim()
-  const theme = { ...baseTheme, ...langTheme[language] }
   const css = highlightLines && highlighLinesStyle(highlightLines)
 
   return (
@@ -305,12 +289,12 @@ const CodeEditor = props => {
       ActionComponent={ActionComponent}
       {...restProps}
     >
-      <TerminalTextWrapper dark>
+      <TerminalTextWrapper theme={theme}>
         <CustomSyntaxHighlighter
           lineNumberStyle={{ color: '#6272A4' }}
           showLineNumbers={showLineNumbers}
           language={language}
-          style={theme}
+          style={{ ...baseTheme, ...langTheme[language] }}
           wrapLines
           children={text}
           css={css}
@@ -321,14 +305,13 @@ const CodeEditor = props => {
 }
 
 CodeEditor.defaultProps = {
+  interactive: false,
   prettier: true,
   showLineNumbers: false,
-  height: 'inherit',
-  width
+  width: TERMINAL_WIDTH,
+  theme: 'dark'
 }
 
 CodeEditor.COLORS = COLORS
-CodeEditor.width = width
-CodeEditor.height = height
 
 export default CodeEditor
