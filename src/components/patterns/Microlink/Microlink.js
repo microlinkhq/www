a69ba1b4a6cb951d8778d5a0113752b82c889c22
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-02-16 10:36:36 +0100

    build: prevent media video under slow connections

--- src/components/patterns/Microlink/Microlink.js
+++ src/components/patterns/Microlink/Microlink.js
@@ -1,13 +1,27 @@
 import React from 'react'
-import { template } from 'helpers'
+import { template, isSSR } from 'helpers'
 import Microlink from '@microlink/react'
+import get from 'dlv'
+
+const effectiveType = isSSR
+  ? undefined
+  : get(window, 'navigator.connection.effectiveType')
+
+const isFastConnection = effectiveType === '4g'
+
+const media = [
+  isFastConnection && 'video',
+  isFastConnection && 'audio',
+  'image',
+  'logo'
+].filter(Boolean)
 
 export default ({ url, style, ...props }) => {
   if (url) url = template(url)
   return (
     <Microlink
       url={url}
-      media={['video', 'audio', 'image', 'logo']}
+      media={media}
       style={{ margin: 'auto', ...style }}
       {...props}
     />
