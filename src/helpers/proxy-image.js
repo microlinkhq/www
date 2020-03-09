2009a005d6533c8cf7eba4f200d93797ae804b67
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-03-09 23:20:46 +0100

    fix: remove lock

--- src/helpers/proxy-image.js
+++ src/helpers/proxy-image.js
@@ -1,7 +1,6 @@
 export default (url, opts) => {
   const encodedUrl = encodeURIComponent(url)
   const query = new URLSearchParams(opts).toString()
-  console.log({ query })
   let proxyUrl = `https://images.weserv.nl/?url=${encodedUrl}&l=9&af&il&n=-1`
   if (query) proxyUrl = `${proxyUrl}&${query}`
   return proxyUrl
