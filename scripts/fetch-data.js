b599a80d80f3cdc75fcab37fa3984fc3147d5e22
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-03-30 23:31:48 +0200

    fix: identity as default mapper

--- scripts/fetch-data.js
+++ scripts/fetch-data.js
@@ -1,19 +1,25 @@
 'use strict'
 
-const { noop, castArray, isEmpty } = require('lodash')
+const { identity, castArray, isEmpty } = require('lodash')
 const beautyError = require('beauty-error')
 const existsFile = require('exists-file')
 const jsonFuture = require('json-future')
 const got = require('got')
 
-module.exports = async ({ dist, url, mapper = noop }) => {
+const fetchData = async url => {
+  const body = await got(url).json()
+  if (!body) throw new Error('DATA_NOT_FOUND')
+}
+
+module.exports = async ({ dist, url, mapper = identity }) => {
   try {
     if (await existsFile(dist)) {
       if (!isEmpty(require(dist))) {
         return
       }
     }
-    const body = mapper(await got(url).json())
+    const data = await fetchData(url)
+    const body = mapper(data)
     return jsonFuture.saveAsync(dist, castArray(body))
   } catch (err) {
     console.log(beautyError(err))
