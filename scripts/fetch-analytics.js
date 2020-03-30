49270745762e79350bbef005c16e781a10a54554
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-03-30 23:39:37 +0200

    fix: update url

--- scripts/fetch-analytics.js
+++ scripts/fetch-analytics.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 require('./fetch-data')({
   dist: path.resolve(__dirname, '../data/analytics.json'),
-  url: 'https://analytics.microlink.io',
+  url: 'https://analytics.microlink.now.sh/',
   mapper: data => {
     const lastMonth = Object.keys(data.byMonth)[0]
     return [data.byMonth[lastMonth]]
