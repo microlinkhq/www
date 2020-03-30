718c2ffe225a2baf29026de464e560f504b1f439
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-03-31 00:04:23 +0200

    fix: component file name

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -2,6 +2,6 @@ import Hero from './hero'
 import Screenshots from './screenshots'
 import Meta from './meta'
 import Explore from './explore'
-import Analytics from './Analytics'
+import Analytics from './analytics'
 
 export { Hero, Screenshots, Meta, Explore, Analytics }
