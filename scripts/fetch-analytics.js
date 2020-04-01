f850ec269713488f7003df52a12809e8334c3b0a
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-04-01 11:38:44 +0200

    build: get metrics from the previous month

--- scripts/fetch-analytics.js
+++ scripts/fetch-analytics.js
@@ -6,7 +6,7 @@ require('./fetch-data')({
   dist: path.resolve(__dirname, '../data/analytics.json'),
   url: 'https://analytics.microlink.io',
   mapper: data => {
-    const lastMonth = Object.keys(data.byMonth)[0]
+    const lastMonth = Object.keys(data.byMonth)[1]
     return [data.byMonth[lastMonth]]
   }
 })
