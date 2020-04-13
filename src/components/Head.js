3959bddfade4c75e4401216a4ac21635c0774eaf
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-04-13 10:45:27 +0200

    build: serve favicon from CDN

--- src/components/Head.js
+++ src/components/Head.js
@@ -95,32 +95,96 @@ function Head (props) {
       {...props}
     >
       <link
-        rel='apple-touch-icon'
-        sizes='180x180'
-        href='/apple-touch-icon.png'
+        rel='apple-touch-icon-precomposed'
+        sizes='57x57'
+        href='https://cdn.microlink.io/logo/apple-touch-icon-57x57.png'
       />
       <link
-        rel='apple-touch-icon'
-        sizes='192x192'
-        href='/android-chrome-192x192.png'
+        rel='apple-touch-icon-precomposed'
+        sizes='114x114'
+        href='https://cdn.microlink.io/logo/apple-touch-icon-114x114.png'
       />
       <link
-        rel='apple-touch-icon'
-        sizes='512x512'
-        href='/android-chrome-512x512.png'
+        rel='apple-touch-icon-precomposed'
+        sizes='72x72'
+        href='https://cdn.microlink.io/logo/apple-touch-icon-72x72.png'
       />
-      <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
+      <link
+        rel='apple-touch-icon-precomposed'
+        sizes='144x144'
+        href='https://cdn.microlink.io/logo/apple-touch-icon-144x144.png'
+      />
+      <link
+        rel='apple-touch-icon-precomposed'
+        sizes='60x60'
+        href='https://cdn.microlink.io/logo/apple-touch-icon-60x60.png'
+      />
+      <link
+        rel='apple-touch-icon-precomposed'
+        sizes='120x120'
+        href='https://cdn.microlink.io/logo/apple-touch-icon-120x120.png'
+      />
+      <link
+        rel='apple-touch-icon-precomposed'
+        sizes='76x76'
+        href='https://cdn.microlink.io/logo/apple-touch-icon-76x76.png'
+      />
+      <link
+        rel='apple-touch-icon-precomposed'
+        sizes='152x152'
+        href='https://cdn.microlink.io/logo/apple-touch-icon-152x152.png'
+      />
       <link
         rel='icon'
         type='image/png'
+        href='https://cdn.microlink.io/logo/favicon-196x196.png'
+        sizes='196x196'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        href='https://cdn.microlink.io/logo/favicon-96x96.png'
+        sizes='96x96'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        href='https://cdn.microlink.io/logo/favicon-32x32.png'
         sizes='32x32'
-        href='/favicon-32x32.png'
       />
       <link
         rel='icon'
         type='image/png'
+        href='https://cdn.microlink.io/logo/favicon-16x16.png'
         sizes='16x16'
-        href='/favicon-16x16.png'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        href='https://cdn.microlink.io/logo/favicon-128.png'
+        sizes='128x128'
+      />
+      <meta name='application-name' content='&nbsp;' />
+      <meta name='msapplication-TileColor' content='#FFFFFF' />
+      <meta
+        name='msapplication-TileImage'
+        content='https://cdn.microlink.io/logo/mstile-144x144.png'
+      />
+      <meta
+        name='msapplication-square70x70logo'
+        content='https://cdn.microlink.io/logo/mstile-70x70.png'
+      />
+      <meta
+        name='msapplication-square150x150logo'
+        content='https://cdn.microlink.io/logo/mstile-150x150.png'
+      />
+      <meta
+        name='msapplication-wide310x150logo'
+        content='https://cdn.microlink.io/logo/mstile-310x150.png'
+      />
+      <meta
+        name='msapplication-square310x310logo'
+        content='https://cdn.microlink.io/logo/mstile-310x310.png'
       />
     </Helmet>
   )
