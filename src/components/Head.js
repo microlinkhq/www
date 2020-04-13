62aabc9e87b17e0ccd7b2df396644750f8b85037
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-04-13 11:07:14 +0200

    build: update dependencies

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet'
 import { useSiteMetadata } from 'components/hook'
 
 const mergeMeta = (props, metadata) => {
