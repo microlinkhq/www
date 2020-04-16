1ec6587772f00accd9bf01b301be9818ac21ae41
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-04-16 23:43:06 +0200

    fix: update query under set

--- src/components/hook/use-query-state.js
+++ src/components/hook/use-query-state.js
@@ -1,28 +1,32 @@
 import stringify from 'fast-safe-stringify'
 import { useState, useEffect } from 'react'
+import { flatten, unflatten } from 'flat'
 import { decode, encode } from 'qss'
 import { navigate } from 'gatsby'
 import { isSSR } from 'helpers'
-import flatten, { unflatten } from 'flat'
 
 const eq = (str1, str2) => stringify(str1) === stringify(str2)
 
 const fromLocation = isSSR
   ? () => ({})
-  : () => unflatten(decode(window.location.search.substring(1)))
+  : () => decode(window.location.search.substring(1))
 
 const condition = isSSR ? [] : [window.location.search]
 
 export const useQueryState = () => {
-  const [query, setQuery] = useState(fromLocation())
+  const [query, setQuery] = useState(unflatten(fromLocation()))
 
   useEffect(() => {
     const newQuery = fromLocation()
     if (!eq(query, newQuery)) setQuery(newQuery)
   }, condition)
 
-  const set = (obj = {}, { navigate: isNavigate = true } = {}) => {
-    const newQuery = flatten({ ...fromLocation(), ...obj })
+  const set = (
+    obj = {},
+    { replace = false, navigate: isNavigate = true } = {}
+  ) => {
+    const newQuery = flatten(replace ? obj : { ...fromLocation(), ...obj })
+    setQuery(newQuery)
     if (isNavigate) navigate(`${window.location.pathname}?${encode(newQuery)}`)
   }
 
