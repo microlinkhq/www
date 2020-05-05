7245265093cbed88a553e26e74726665452637bc
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-05-05 21:55:35 +0200

    fix: icon under non domain

--- src/components/elements/Input/InputIcon.js
+++ src/components/elements/Input/InputIcon.js
@@ -1,15 +1,14 @@
-import React from 'react'
-
 import { Box, Unavatar } from 'components/elements'
 import { Link as LinkIcon } from 'react-feather'
-import { colors } from 'theme'
+import { toPx, colors } from 'theme'
+import React from 'react'
 
-export default ({ width = '24px', height = '100%', domain }) => {
+export default ({ width = 24, domain }) => {
   const children = domain ? (
-    <Unavatar query={domain} width={width} height={height} />
+    <Unavatar query={domain} width={toPx(width)} />
   ) : (
-    <LinkIcon color={colors.black50} size={width} />
+    <LinkIcon color={colors.black50} size={toPx(width / 2)} />
   )
 
-  return <Box width={width} pl={2} children={children} />
+  return <Box pt={domain ? 0 : 1} width={width} pl={2} children={children} />
 }
