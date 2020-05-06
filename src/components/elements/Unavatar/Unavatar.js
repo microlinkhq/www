2082fd5826ebc57fc2fe7a1c2f3b7f675eaa8cf0
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-05-07 00:28:02 +0200

    build: add border radius

--- src/components/elements/Unavatar/Unavatar.js
+++ src/components/elements/Unavatar/Unavatar.js
@@ -3,6 +3,7 @@ import { Image } from 'components/elements'
 
 export default ({ from = '', query, ...props }) => (
   <Image
+    borderRadius={2}
     src={`https://unavatar.now.sh/${from ? `${from}/${query}` : query}`}
     css={`
       max-width: inherit;
