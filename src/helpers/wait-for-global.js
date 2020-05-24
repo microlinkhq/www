e53212d850209f3b67c3de3e96eb2d161113a397
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-05-25 01:19:55 +0200

    fix: only load waitForGlobal on non SSR

--- src/helpers/wait-for-global.js
+++ src/helpers/wait-for-global.js
@@ -1,5 +1,9 @@
+import isSSR from './is-ssr'
+
+const store = isSSR ? {} : window
+
 function waitForGlobal (key, cb, time = 100) {
-  return window[key]
+  return store[key]
     ? cb()
     : setTimeout(() => waitForGlobal(key, cb, time), time)
 }
