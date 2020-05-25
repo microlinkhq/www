adf9db6488c96cb929081d4f4da5097b622bc330
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-05-25 11:24:28 +0200

    build(debug): traces

--- src/components/elements/Runkit/Runkit.js
+++ src/components/elements/Runkit/Runkit.js
@@ -1,28 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { waitForGlobal } from 'helpers'
 
 export default ({ nodeVersion = '12', loader, children, ...props }) => {
   const loaderChildren = loader()
 
-  waitForGlobal('RunKit', () => {
-    const loaderId = loaderChildren.props.id
-    const el = document.getElementById(loaderId)
-    if (el && !el.querySelector('iframe')) {
-      window.RunKit.createNotebook({
-        element: el,
-        name: el.id,
-        title: el.children[0].textContent,
-        source: el.children[1].textContent,
-        nodeVersion: '14',
-        gutterStyle: 'outside',
-        onLoad: () => {
-          el.children[0].style.display = 'none'
-          el.children[1].style.display = 'none'
-          el.style['box-shadow'] = 'none'
-        }
-      })
-    }
-  })
+  useEffect(() => {
+    waitForGlobal('RunKit', () => {
+      console.log('[runkit] loaded')
+      const loaderId = loaderChildren.props.id
+      const el = document.getElementById(loaderId)
+      console.log('[runkit]', !!el)
+      if (el && !el.querySelector('iframe')) {
+        console.log('[runkit]', el.id)
+        const result = window.RunKit.createNotebook({
+          element: el,
+          name: el.id,
+          title: el.children[0].textContent,
+          source: el.children[1].textContent,
+          nodeVersion: '14',
+          gutterStyle: 'outside',
+          onLoad: () => {
+            el.children[0].style.display = 'none'
+            el.children[1].style.display = 'none'
+            el.style['box-shadow'] = 'none'
+          }
+        })
+        console.log('[runkit] result', result)
+      }
+    })
+  }, [])
 
   return <>{loaderChildren}</>
 }
