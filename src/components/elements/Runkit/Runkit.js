4a8df4cb92bec2f941b2a057d783297db6b7a5bd
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-05-26 00:42:05 +0200

    build: destroy notebook on unmount

--- src/components/elements/Runkit/Runkit.js
+++ src/components/elements/Runkit/Runkit.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { waitForGlobal } from 'helpers'
 
 export default ({ nodeVersion = '12', loader, children, ...props }) => {
+  const [notebook, setNotebook] = useState(null)
   const loaderChildren = loader()
 
   useEffect(() => {
@@ -9,19 +10,23 @@ export default ({ nodeVersion = '12', loader, children, ...props }) => {
       const loaderId = loaderChildren.props.id
       const el = document.getElementById(loaderId)
       if (el && !el.querySelector('iframe')) {
-        window.RunKit.createNotebook({
-          element: el,
-          name: el.id,
-          title: el.children[0].textContent,
-          source: el.children[1].textContent,
-          nodeVersion: '14',
-          gutterStyle: 'outside',
-          onLoad: () => {
-            el.children[0].style.display = 'none'
-            el.children[1].style.display = 'none'
-            el.style['box-shadow'] = 'none'
-          }
-        })
+        setNotebook(
+          window.RunKit.createNotebook({
+            element: el,
+            name: el.id,
+            title: el.children[0].textContent,
+            source: el.children[1].textContent,
+            nodeVersion: '14',
+            gutterStyle: 'outside',
+            onLoad: () => {
+              el.children[0].style.display = 'none'
+              el.children[1].style.display = 'none'
+              el.style['box-shadow'] = 'none'
+            }
+          })
+        )
+
+        return () => notebook.destroy()
       }
     })
   }, [])
