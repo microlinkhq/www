1cebd2c43d2c53574a6ad848a5fa638b90ad9bc5
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-05-25 17:51:49 +0200

    refactor: use react hooks

--- src/components/patterns/MQLEditor/MQLEditor.js
+++ src/components/patterns/MQLEditor/MQLEditor.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Terminal, MultiCodeEditor, Card, Flex } from 'components/elements'
 
 import languages, { tabs } from './languages'
 
 const TABS = Object.keys(tabs)
 
-export default class extends Component {
-  state = { view: TABS[0] }
+export default props => {
+  const [view, setView] = useState(TABS[0])
 
-  render () {
-    const { view } = this.state
-
-    return (
-      <Flex mt={3} flexDirection='column' justifyContent='space-around'>
-        <Flex flexDirection='column' mb={[4, 0]}>
-          <MultiCodeEditor
-            height={Terminal.height}
-            languages={languages(view)}
-          />
-          <Flex justifyContent='flex-end'>
-            {TABS.map(tab => (
-              <Card.Option
-                key={tab}
-                children={tab}
-                value={view}
-                onClick={() => this.setState({ view: tab })}
-              />
-            ))}
-          </Flex>
+  return (
+    <Flex
+      mt={3}
+      flexDirection='column'
+      justifyContent='space-around'
+      {...props}
+    >
+      <Flex flexDirection='column' mb={[4, 0]}>
+        <MultiCodeEditor height={Terminal.height} languages={languages(view)} />
+        <Flex justifyContent='flex-end'>
+          {TABS.map(tab => (
+            <Card.Option
+              key={tab}
+              children={tab}
+              value={view}
+              onClick={() => setView(tab)}
+            />
+          ))}
         </Flex>
       </Flex>
-    )
-  }
+    </Flex>
+  )
 }
