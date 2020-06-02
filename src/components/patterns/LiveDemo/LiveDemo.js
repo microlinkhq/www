562b764c765615c5b76b27fb3c676413d8ec8151
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-06-02 11:06:58 +0200

    fix: remove unnecessary reference

--- src/components/patterns/LiveDemo/LiveDemo.js
+++ src/components/patterns/LiveDemo/LiveDemo.js
@@ -1,26 +1,14 @@
 import React, { useState } from 'react'
-
-import { CodeEditor, Card, Flex } from 'components/elements'
+import { Card, Flex } from 'components/elements'
 import Preview from './preview'
-
-const { colors: COLORS } = CodeEditor
 
 export default ({ children, loading }) => {
   const [view, setView] = useState('preview')
-  const isSDK = view === 'preview'
 
   return (
     <Flex flexDirection='column' justifyContent='space-around'>
       <Flex flexDirection='column' mb={[4, 0]}>
-        <Card
-          px={isSDK ? 0 : 3}
-          py={isSDK ? 0 : 3}
-          style={{
-            background: isSDK ? '#fff' : COLORS.BACKGROUND,
-            padding: 0,
-            overflow: isSDK ? 'hidden' : 'auto'
-          }}
-        >
+        <Card>
           <Preview loading={loading} view={view} children={children} />
         </Card>
         <Flex as='footer' justifyContent='flex-end'>
