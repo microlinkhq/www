e1ecfb2da2c89b254d35de0ae2bfc49529611ba7
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-06-15 19:45:18 +0200

    fix: application name

--- src/components/Head.js
+++ src/components/Head.js
@@ -261,7 +261,7 @@ function Head ({ onChangeClientState, script, ...props }) {
         href='https://cdn.microlink.io/logo/favicon-128.png'
         sizes='128x128'
       />
-      <meta name='application-name' content='&nbsp;' />
+      <meta name='application-name' content='microlink.io' />
       <meta name='msapplication-TileColor' content='#FFFFFF' />
       <meta
         name='msapplication-TileImage'
