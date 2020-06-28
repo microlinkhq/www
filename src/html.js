d53b24f26bcc0bba43629d85508a8eebec255a62
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-06-28 11:27:02 +0200

    build: load as link

--- src/html.js
+++ src/html.js
@@ -11,6 +11,10 @@ export default function HTML (props) {
           name='viewport'
           content='width=device-width, initial-scale=1, shrink-to-fit=no'
         />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Inter:wght@100;200;400;500;600&display=swap'
+          rel='stylesheet'
+        />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
