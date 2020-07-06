53755f5b6ea8a4f1bdc8e22e640904e9e4bea479
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-07-07 01:16:39 +0200

    refactor: simplify layout

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,7 +1,6 @@
 import Hero from './hero'
 import Screenshots from './screenshots'
 import Meta from './meta'
-import Explore from './explore'
 import Analytics from './analytics'
 
-export { Hero, Screenshots, Meta, Explore, Analytics }
+export { Hero, Screenshots, Meta, Analytics }
