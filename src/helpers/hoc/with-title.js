cddaaea145366a9e87177ebe45030e8372e6e39c
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-07-24 11:11:26 +0200

    refactor: return early

--- src/helpers/hoc/with-title.js
+++ src/helpers/hoc/with-title.js
@@ -2,14 +2,11 @@ import { createElement } from 'react'
 import { title as titleize } from 'helpers'
 
 export const withTitle = Component => props => {
-  if (typeof props.children !== 'string') {
+  if (typeof props.children !== 'string' || props.titleize === false) {
     return createElement(Component, props)
   }
 
-  const title =
-    props.titleize !== false
-      ? titleize(props.children, props.omitTitleize)
-      : props.children
+  const title = titleize(props.children, props.omitTitleize)
 
   return createElement(Component, {
     ...props,
