576b9215ff0210b55493c42e9aa138ae6f1bbe0a
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-07-26 14:04:54 +0200

    build: setup fallback value

--- src/components/hook/use-window-size.js
+++ src/components/hook/use-window-size.js
@@ -2,14 +2,14 @@ import { useState, useEffect } from 'react'
 import { isSSR } from 'helpers'
 
 const getSize = isSSR
-  ? () => ({})
+  ? fallback => fallback
   : () => ({ width: window.innerWidth, height: window.innerHeight })
 
-export function useWindowSize () {
-  const [windowSize, setWindowSize] = useState(getSize)
+export function useWindowSize (fallback) {
+  const [windowSize, setWindowSize] = useState(getSize(fallback))
 
   useEffect(() => {
-    const handleResize = () => setWindowSize(getSize())
+    const handleResize = () => setWindowSize(getSize(fallback))
     window.addEventListener('resize', handleResize)
     return () => window.removeEventListener('resize', handleResize)
   }, [])
