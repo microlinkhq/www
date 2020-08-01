cf1af7686047a198eaf35be1530306eb5c04f003
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-08-01 17:23:49 +0200

    build: migrate /changelog

--- src/pages/changelog.js
+++ src/pages/changelog.js
@@ -1,6 +1,6 @@
-import { Headline, Layout } from 'components/patterns'
-import { Flex, Text, Box } from 'components/elements'
-import Markdown, { H1 } from 'components/markdown'
+import { Caption, Layout } from 'components/patterns'
+import { Heading, Flex, Box } from 'components/elements'
+import Markdown, { H2 } from 'components/markdown'
 import { useChangelog } from 'components/hook'
 import { layout } from 'theme'
 import React from 'react'
@@ -18,19 +18,26 @@ export default ({ meta }) => {
         justifyContent='center'
         alignItems='center'
       >
-        <Headline title='Changelog' pb={3} />
-        <Text
-          fontSize={[2, 2, 2, 3]}
-          mb={4}
-          textAlign='center'
-          maxWidth={10}
+        <Heading maxWidth={layout.large} children='Changelog' />
+
+        <Caption
+          pt={[3, 3, 4, 4]}
+          px={[4, 4, 0, 0]}
+          titleize={false}
+          maxWidth={layout.small}
           children='We’re constantly improving the platform. See here notable changes in our lineup of products & improvements over the time.'
         />
-        <Flex width='100%' maxWidth={layout.large} flexDirection='column'>
+
+        <Flex
+          pt={[3, 3, 4, 4]}
+          swidth='100%'
+          maxWidth={layout.large}
+          flexDirection='column'
+        >
           {changelog.map(({ date, notes }) => {
             return (
               <Box key={date}>
-                <H1 mb={0} children={date} />
+                <H2 children={date} />
                 <Markdown
                   children={notes.map(note => `- ${note}`).join('\n')}
                 />
