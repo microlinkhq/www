d517095999bd6c9a8bc07bbe465f0d7d78bd0f5f
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-08-01 17:14:44 +0200

    build: migrate /oss

--- src/pages/oss.js
+++ src/pages/oss.js
@@ -1,6 +1,6 @@
 import { Star as StarIcon, AlertCircle as IssueIcon } from 'react-feather'
-import { Headline, DotsBackground, Layout } from 'components/patterns'
-import { Link, Text, Flex, Box } from 'components/elements'
+import { Caption, DotsBackground, Layout } from 'components/patterns'
+import { Heading, Link, Text, Flex, Box } from 'components/elements'
 import { useOss } from 'components/hook'
 import { layout } from 'theme'
 import React from 'react'
@@ -18,15 +18,26 @@ export default () => {
           justifyContent='center'
           alignItems='center'
         >
-          <Headline title='Open Source Software' pb={3} />
-          <Text
-            fontSize={[2, 2, 2, 3]}
-            mb={4}
-            textAlign='center'
-            maxWidth={10}
+          <Heading
+            titleize={false}
+            maxWidth={layout.large}
+            children='Open Source Software'
+          />
+
+          <Caption
+            pt={[3, 3, 4, 4]}
+            px={[4, 4, 0, 0]}
+            titleize={false}
+            maxWidth={layout.small}
             children="It's our great privilege to build our products using open source software (OSS) and we want to give the same effort back."
           />
-          <Flex width='100%' maxWidth={layout.large} flexDirection='column'>
+
+          <Flex
+            pt={[3, 3, 4, 4]}
+            width='100%'
+            maxWidth={layout.large}
+            flexDirection='column'
+          >
             {repos.map(({ name, description, stars, issues, url }) => (
               <Box key={name} mb={3} borderBottom={1} borderColor='black05'>
                 <Link color='black' href={url}>
@@ -34,7 +45,7 @@ export default () => {
                     fontWeight='bold'
                     mr={3}
                     display='inline-block'
-                    width='150px'
+                    width='180px'
                     children={name}
                   />
                   <Flex color='black' style={{ float: 'right' }}>
@@ -47,7 +58,7 @@ export default () => {
                       <IssueIcon width='16px' />
                     </Flex>
                   </Flex>
-                  <Text color='black50' children={description} mb={2} />
+                  <Text color='black50' children={description} mt={2} mb={2} />
                 </Link>
               </Box>
             ))}
