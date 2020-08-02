3a042dc6db85eea9c76ebc6802e9f30c1104a6d3
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-08-02 23:43:02 +0200

    build: migrate /stats

--- src/components/patterns/ClusterMonitor/ClusterMonitor.js
+++ src/components/patterns/ClusterMonitor/ClusterMonitor.js
@@ -11,7 +11,7 @@ const ClusterMonitor = ({ children, ...opts }) => {
 
   return children({
     data,
-    isLoading: !!data
+    isLoading: !data
   })
 }
 
--- src/pages/stats.js
+++ src/pages/stats.js
@@ -33,41 +33,40 @@ export default () => {
             component={Flex}
             justifyContent='center'
             alignItems='center'
+            maxWidth='100%'
           >
-            <Container maxWidth='100%' px={3}>
-              <Box id='stats'>
-                {isLoading ? (
-                  <>
-                    <Text
-                      pt={[0, 0, 0, 3]}
-                      color={color}
-                      textAlign='center'
-                      style={{ fontFamily: 'monospace' }}
-                    >
-                      $ watch curl -sL https://k8s.microlink.io/
-                    </Text>
-                    <Text
-                      style={{ whiteSpace: 'pre', fontFamily: 'monospace' }}
-                      color={color}
-                      fontSize={[0, 0, 1, 1]}
-                      children={`\n${data}`}
-                    />
-                  </>
-                ) : (
-                  <>
-                    <Flex
-                      justifyContent='center'
-                      alignItems='center'
-                      flexDirection={['column', 'column', 'row', 'row']}
-                    >
-                      <Value color={color}>
-                        Loading <DotSpinner />
-                      </Value>
-                    </Flex>
-                  </>
-                )}
-              </Box>
-            </Container>
+            <Box id='stats' px={Container.defaultProps.px} pt={5}>
+              {isLoading ? (
+                <>
+                  <Flex
+                    justifyContent='center'
+                    alignItems='center'
+                    flexDirection={['column', 'column', 'row', 'row']}
+                  >
+                    <Value color={color}>
+                      Loading <DotSpinner />
+                    </Value>
+                  </Flex>
+                </>
+              ) : (
+                <>
+                  <Text
+                    color={color}
+                    textAlign='center'
+                    style={{ fontFamily: 'monospace' }}
+                  >
+                    $ watch curl -sL https://k8s.microlink.io/
+                  </Text>
+                  <Text
+                    textAlign='center'
+                    style={{ whiteSpace: 'pre', fontFamily: 'monospace' }}
+                    color={color}
+                    fontSize={[0, 0, 1, 1]}
+                    children={`\n${data}`}
+                  />
+                </>
+              )}
+            </Box>
           </Layout>
         )
       }}
