3fdeeb9ddca5f643441d5b864c18879ff29b49de
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-09-17 00:30:01 +0200

    build: remove unnecessary files

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,7 +1,6 @@
-import Screenshots from './screenshots'
 import Analytics from './analytics'
 import Hero from './hero'
 import Pricing from './pricing'
 import Faqs from './faqs'
 
-export { Faqs, Hero, Screenshots, Analytics, Pricing }
+export { Faqs, Hero, Analytics, Pricing }
