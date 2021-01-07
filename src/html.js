3b55f61a27f7f4950d7a9329e84a6ca78c4b095e
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-01-07 11:25:11 +0100

    build: add crossorigin google fonts

--- src/html.js
+++ src/html.js
@@ -6,6 +6,11 @@ export default function HTML (props) {
     <html lang='en' {...props.htmlAttributes}>
       <head>
         <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
+        <link
           href='https://fonts.googleapis.com/css2?family=Inter:wght@100;200;400;500;600&display=swap'
           rel='stylesheet'
         />
