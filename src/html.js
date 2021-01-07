d542a40c947db9ef5ed334763ce8c7b47e2f6d3d
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-01-07 12:11:03 +0100

    build: optimize fonts

--- src/html.js
+++ src/html.js
@@ -8,11 +8,14 @@ export default function HTML (props) {
         <link
           rel='preconnect'
           href='https://fonts.gstatic.com'
-          crossOrigin='true'
+          crossOrigin='anonymous'
         />
         <link
           href='https://fonts.googleapis.com/css2?family=Inter:wght@100;200;400;500;600&display=swap'
           rel='stylesheet'
+          as='font'
+          type='font/woff2'
+          crossOrigin='anonymous'
         />
         {props.headComponents}
       </head>
