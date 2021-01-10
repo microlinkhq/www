43fdc343ebcd6fefcfce9b468d41b333bac50c4e
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-01-10 23:03:57 +0100

    build: remove debug

--- scripts/fetch-data/create-provider.js
+++ scripts/fetch-data/create-provider.js
@@ -7,7 +7,6 @@ const pRetry = require('p-retry')
 const got = require('got')
 
 const fetchData = async url => {
-  console.log({ url })
   const body = await got(url).json()
   if (isEmpty(body)) throw new Error('DATA_NOT_FOUND')
   return body
@@ -20,15 +19,6 @@ module.exports = async ({ dist, url, mapper = identity }) => {
     }
   }
   const data = await pRetry(() => fetchData(url))
-  try {
-    console.log(url, {
-      type: typeof data,
-      data: !!data,
-      preview: data.substring(1, 5)
-    })
-  } catch (err) {
-    console.log('ERR', data)
-  }
   const body = mapper(data)
   return jsonFuture.saveAsync(dist, castArray(body))
 }
