5973aae1465a53f113dc20f731e3424c3e0b1930
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-05-22 17:15:09 +0200

    build: better storybook 6 integration

---
+++ .storybook/main.js
@@ -0,0 +1,96 @@
+const path = require('path')
+
+module.exports = {
+  stories: ['../src/**/*.stories.js'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-viewport',
+    '@storybook/addon-actions'
+  ],
+  core: {
+    builder: 'webpack5'
+  },
+  webpackFinal: async config => {
+    // https://www.gatsbyjs.com/docs/how-to/testing/visual-testing-with-storybook/
+    config.module.rules.push({
+      test: /\.(js)$/,
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [
+              // use @babel/preset-react for JSX and env (instead of staged presets)
+              require.resolve('@babel/preset-react'),
+              require.resolve('@babel/preset-env')
+            ],
+            plugins: [
+              // use @babel/plugin-proposal-class-properties for class arrow functions
+              require.resolve('@babel/plugin-proposal-class-properties'),
+              // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+              require.resolve('babel-plugin-remove-graphql-queries'),
+              // use babel-plugin-react-docgen to ensure PropTables still appear
+              require.resolve('babel-plugin-react-docgen')
+            ]
+          }
+        }
+      ],
+      exclude: [/node_modules\/(?!(gatsby)\/)/]
+    })
+
+    config.module.rules.push({
+      test: /\.s[ac]ss$/i,
+      oneOf: [
+        // module.scss files (e.g component styles.module.scss)
+        // https://webpack.js.org/loaders/style-loader/#modules
+        {
+          test: /\.module\.s?css$/,
+          use: [
+            // Add exports of a module as style to DOM
+            {
+              loader: 'style-loader',
+              options: {
+                esModule: true,
+                modules: {
+                  namedExport: true
+                }
+              }
+            },
+            // Loads CSS file with resolved imports and returns CSS code
+            {
+              loader: 'css-loader',
+              options: {
+                esModule: true,
+                modules: {
+                  namedExport: true
+                }
+              }
+            },
+            // Loads and compiles a SASS/SCSS file
+            'sass-loader'
+          ]
+        },
+        // scss files that are not modules (e.g. custom.scss)
+        {
+          use: [
+            // Add exports of a module as style to DOM
+            'style-loader',
+            // Loads CSS file with resolved imports and returns CSS code
+            'css-loader',
+            // Loads and compiles a SASS/SCSS file
+            'sass-loader'
+          ]
+        }
+      ]
+    })
+
+    // Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
+    config.resolve.mainFields = ['browser', 'module', 'main']
+
+    config.resolve.modules = [
+      ...config.resolve.modules,
+      path.resolve(__dirname, '../src')
+    ]
+
+    return config
+  }
+}
---
+++ .storybook/preview.js
@@ -0,0 +1,36 @@
+import { ThemeProvider } from 'styled-components'
+import Flex from 'components/elements/Flex'
+import React from 'react'
+import theme from 'theme'
+
+import '@storybook/addon-console'
+
+// Gatsby's Link overrides:
+// Gatsby defines a global called ___loader to prevent its method calls from creating console errors you override it here
+global.___loader = {
+  enqueue: () => {},
+  hovering: () => {}
+}
+
+// Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
+global.__BASE_PATH__ = '/'
+
+// This is to utilized to override the window.___navigate method Gatsby defines and uses to report what path a Link would be taking us to if it wasn't inside a storybook
+window.___navigate = pathname => {
+  action('NavigateTo:')(pathname)
+}
+
+export const decorators = [
+  Story => (
+    <ThemeProvider theme={theme}>
+      <Flex
+        p={3}
+        justiContent='center'
+        alignItems='baseline'
+        flexDirection='column'
+      >
+        <Story />
+      </Flex>
+    </ThemeProvider>
+  )
+]
