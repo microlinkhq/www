296028b55595b3ee98f214a991cf4ab3d5887233
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-05-22 17:15:09 +0200

    build: better storybook 6 integration

--- .storybook/addons.js
+++
@@ -1,3 +0,0 @@
-import '@storybook/addon-a11y/register'
-import '@storybook/addon-viewport/register'
-import '@storybook/addon-actions/register'
---
+++ .storybook/theme.js
@@ -0,0 +1,7 @@
+import { create } from '@storybook/theming'
+
+export default create({
+  base: 'light',
+  brandTitle: 'Microlink Design',
+  brandUrl: 'https://github.com/microlinkhq/www'
+})
