8175cd35b1ecfbf10fe62297284eabba2f4b9910
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-06-23 18:40:32 +0200

    chore: add enterprise page

---
+++ src/pages/enterprise.js
@@ -0,0 +1,99 @@
+import { DotsBackground, Caption, Layout } from 'components/patterns'
+import { transition, space, gradient, layout } from 'theme'
+import Markdown from 'components/markdown'
+import { useTheme } from 'components/hook'
+import styled from 'styled-components'
+import React from 'react'
+
+import {
+  Caps,
+  Button,
+  Subhead,
+  Heading,
+  Box,
+  Container
+} from 'components/elements'
+
+const THEMES = {
+  dark: {
+    primary: 'white',
+    secondary: 'white80'
+  }
+}
+
+const content = `
+The **Microlink Enterprise** plan is oriented for any customer that wants to maximize Microlink capabilities, unleashing the maximum performance with dedicated hardware and superior software integration with any existence customer infrastructure.
+
+Running a **Microlink Enterprise** plan means:
+
+- Your own Microlink API endpoint, with your own dedicated pool of always-ready browsers.
+- Your own S3-like storage service for assets, with no time-to-live (TTL) restriction.
+- Your own CDN distribution (+200 nodes over the world, powered by CloudFlare).
+
+The physical servers associated with your own Microlink API endpoint can be chosen between 8 locations (New York, San Francisco, Amsterdam, Singapore, London, Frankfurt, Toronto, or Bangalore).
+
+The price model is the same, plus 500€ for the extra cost of keeping the dedicated infrastructure running.`.trim()
+
+const GradientButton = styled(Button)`
+  transition: filter ${transition.medium};
+  background-image: ${gradient};
+  padding: ${space[1]};
+
+  &&& {
+    &:hover:not([disabled]) {
+      color: inherit;
+      box-shadow: none;
+      filter: hue-rotate(40deg);
+    }
+  }
+`
+
+const EnterprisePage = () => {
+  const [{ theme, primary, secondary }] = useTheme(THEMES, 'dark')
+
+  return (
+    <DotsBackground theme={theme}>
+      <Layout theme={theme} footer={{ bg: 'transparent' }}>
+        <Container
+          pt={[2, 2, 3, 3]}
+          justifyContent='center'
+          alignItems='center'
+        >
+          <Subhead color={primary}>Microlink for</Subhead>
+          <Heading>Enterprise</Heading>
+          <Caption
+            color={primary}
+            pt={[3, 3, 4, 4]}
+            px={4}
+            titleize={false}
+            maxWidth={[layout.small, layout.small, layout.small, layout.small]}
+          >
+            Unleash the maximum performance. Hardware, with the software baked
+            in.
+          </Caption>
+          <Box pt={[3, 3, 4, 4]} color={secondary}>
+            <Markdown>{content}</Markdown>
+          </Box>
+          <Box pt={[2, 2, 3, 3]} color={secondary}>
+            <GradientButton
+              onClick={event => {
+                event.preventDefault()
+                window.open(
+                  'mailto:hello@microlink.io?subject=Microlink%20Enterprise&body=Hello%2C%20I%20want%20to%20upgrade%20my%20customer%20plan%20to%20Microlink%20Enterprise.%0D%0A%0D%0ACan%20you%20tell%20me%20more%20about%20the%20details%3F%0D%0A%0D%0AThank%20you!%0D%0A',
+                  '_blank',
+                  'noopener noreferrer'
+                )
+              }}
+            >
+              <Caps bg='black' px={3} py={2} color={primary}>
+                Get in touch
+              </Caps>
+            </GradientButton>
+          </Box>
+        </Container>
+      </Layout>
+    </DotsBackground>
+  )
+}
+
+export default EnterprisePage
