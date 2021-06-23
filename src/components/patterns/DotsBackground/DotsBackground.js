a5f7b65abfba8fcd64f7bac8101742bf18eab00c
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-06-23 16:21:26 +0200

    chore: add theme support for DotsBackground

--- src/components/patterns/DotsBackground/DotsBackground.js
+++ src/components/patterns/DotsBackground/DotsBackground.js
@@ -1,7 +1,24 @@
-import styled from 'styled-components'
-import { Box } from 'components/elements'
-
+import styled, { css } from 'styled-components'
 import { slide } from 'components/keyframes'
+import { Box } from 'components/elements'
+import is from 'styled-is'
+
+const darkStyle = css`
+  background: #000;
+  background-color: #000;
+  background-image: radial-gradient(#d7d7d726 1px, transparent 0),
+    radial-gradient(#d7d7d726 1px, transparent 0);
+`
+
+const lightStyle = css`
+  background-image: radial-gradient(#d7d7d7 1px, transparent 0),
+    radial-gradient(#d7d7d7 1px, transparent 0);
+`
+
+const animateStyle = css`
+  animation: ${slide} 100s linear infinite;
+  animation-direction: reverse;
+`
 
 const Dots = styled(Box)`
   position: relative;
@@ -14,14 +31,17 @@ const Dots = styled(Box)`
     height: 100%;
     z-index: -1;
 
-    background-image: radial-gradient(#d7d7d7 1px, transparent 0),
-      radial-gradient(#d7d7d7 1px, transparent 0);
+    ${props => (props.theme === 'dark' ? darkStyle : lightStyle)}
     background-position: 0 0, 25px 25px;
     background-size: 50px 50px;
 
-    animation: ${slide} 100s linear infinite;
-    animation-direction: reverse;
+    ${is('animate')`${animateStyle}`};
   }
 `
+
+Dots.defaultProps = {
+  theme: 'light',
+  animate: true
+}
 
 export default Dots
--- src/components/patterns/DotsBackground/DotsBackground.stories.js
+++ src/components/patterns/DotsBackground/DotsBackground.stories.js
@@ -1,31 +1,49 @@
-import React from 'react'
+import { Button, Box, Text, Flex } from 'components/elements'
 import { storiesOf } from '@storybook/react'
+import React, { useState } from 'react'
 import { Story } from 'story'
-import { Text, Flex } from 'components/elements'
 
 import DotsBackground from './DotsBackground'
 
+const DotsBackgroundStory = () => {
+  const [theme, setTheme] = useState('light')
+  const [animate, setAnimate] = useState(true)
+
+  const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light')
+  const toggleAnimate = () => setAnimate(!animate)
+
+  return (
+    <Story name='DotsBackground' height='100vh' width='100%'>
+      <Box mb={4} width={650}>
+        <Button onClick={toggleTheme}>toggle theme</Button>
+        <Button ml={2} onClick={toggleAnimate}>
+          toggle animation
+        </Button>
+      </Box>
+      <DotsBackground theme={theme} animate={animate}>
+        <Flex
+          px={4}
+          pt={4}
+          pb={4}
+          width='100%'
+          height='50vh'
+          flexDirection='column'
+          justifyContent='center'
+          alignItems='center'
+        >
+          <Text color={theme === 'light' ? 'black' : 'white'}>
+            dots,{' '}
+            <Text as='span' fontWeight='bold'>
+              dots
+            </Text>{' '}
+            everywhere
+          </Text>
+        </Flex>
+      </DotsBackground>
+    </Story>
+  )
+}
+
 storiesOf('Patterns', module).add('DotsBackground', () => (
-  <Story name='DotsBackground' height='100vh' width='100%'>
-    <DotsBackground>
-      <Flex
-        px={4}
-        pt={4}
-        pb={4}
-        width='100%'
-        height='50vh'
-        flexDirection='column'
-        justifyContent='center'
-        alignItems='center'
-      >
-        <Text>
-          dots,{' '}
-          <Text as='span' fontWeight='bold'>
-            dots
-          </Text>{' '}
-          everywhere
-        </Text>
-      </Flex>
-    </DotsBackground>
-  </Story>
+  <DotsBackgroundStory />
 ))
