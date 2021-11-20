71ffc6acbaa00247972b56615e6c392e54979946
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-11-20 23:44:38 +0100

    chore(unavatar): use microlink as provider
    normally it gets better image definition

--- src/components/pages/recipes/index.js
+++ src/components/pages/recipes/index.js
@@ -5,7 +5,7 @@ export const Logo = ({ height, width, isGeneric, domain, logo }) => {
   const sizeProps = { height, width }
 
   if (!isGeneric || !logo) {
-    return <Unavatar query={domain} {...sizeProps} />
+    return <Unavatar query={`microlink/${domain}`} {...sizeProps} />
   }
 
   return <Image alt={`${domain} logo`} src={logo} {...sizeProps} />
