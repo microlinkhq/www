8f5940f0d1e46bc5a628e6700117609187ac0fa8
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-08-29 11:55:51 +0200

    refactor: update codecopy component

--- src/components/elements/Codecopy.js
+++ src/components/elements/Codecopy.js
@@ -4,19 +4,8 @@ import { cx } from 'theme'
 import React from 'react'
 
 const CodeCopyWrapper = styled('div')`
-  .codecopy_button {
-    &:hover {
-      box-shadow: none;
-    }
-
-    svg {
-      fill: ${({ isDark }) => cx(isDark ? 'white50' : 'black20')};
-
-      &:hover {
-        fill: ${({ isDark }) => cx(isDark ? 'white' : 'black')};
-      }
-    }
-
+  .codecopy__button {
+    box-shadow: none;
     background: none;
     border: 0;
     padding: 0;
@@ -24,12 +13,47 @@ const CodeCopyWrapper = styled('div')`
     top: -2px;
     left: 0;
     right: 0;
+
+    &[aria-label='Copied!']::after {
+      right: 30px;
+    }
+
+    &[aria-label='Copy to clipboard']::after {
+      right: 56px;
+    }
+
+    &:hover {
+      background: none;
+    }
+  }
+
+  .codecopy__button__light::before {
+    border-bottom-color: black;
+  }
+  .codecopy__button__light::after {
+    background: black;
+  }
+
+  .codecopy__button__dark::before {
+    border-bottom-color: white;
+  }
+  .codecopy__button__dark::after {
+    background: white;
+    color: black;
+  }
+
+  .codecopy__icon {
+    fill: ${({ isDark }) => cx(isDark ? 'white50' : 'black20')};
+
+    &:hover {
+      fill: ${({ isDark }) => cx(isDark ? 'white' : 'black')};
+    }
   }
 `
 
-const CodeCopy = props => (
-  <CodeCopyWrapper {...props}>
-    <CodeCopyBase interactive {...props} />
+const CodeCopy = ({ isDark, ...props }) => (
+  <CodeCopyWrapper isDark={isDark} {...props}>
+    <CodeCopyBase theme={isDark ? 'dark' : 'light'} interactive {...props} />
   </CodeCopyWrapper>
 )
 
