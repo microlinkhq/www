77781e41ffadadbf52190682548ebbb6f0dccc80
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-01-12 16:57:15 +0100

    refactor: tweaks

--- src/components/patterns/Toolbar/Toolbar.js
+++ src/components/patterns/Toolbar/Toolbar.js
@@ -3,9 +3,7 @@ import ToolbarDesktop from './ToolbarDesktop'
 import ToolbarMobile from './ToolbarMobile'
 import { createElement } from 'react'
 
-const Toolbar = props => {
-  const breakpoint = useBreakpoint()
-  return createElement(breakpoint === 0 ? ToolbarMobile : ToolbarDesktop, props)
-}
+const Toolbar = props =>
+  createElement(useBreakpoint() === 0 ? ToolbarMobile : ToolbarDesktop, props)
 
 export default Toolbar
