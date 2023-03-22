b524f24192043f0c45a0930d69601ec2711e5888
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-03-22 11:01:40 +0100

    test: update snapshot

--- test/helpers/mql-code.js
+++ test/helpers/mql-code.js
@@ -5,16 +5,11 @@ import mqlCode from '../../src/helpers/mql-code'
 test('JavaScript with inline code', t => {
   const output = mqlCode('https://microlink.io', {
     function: "({ page }) => page.evaluate('jQuery.fn.jquery')",
+    number: 3000,
+    boolean: true,
+    string: 'MyApiToken',
+    url: 'https://myproxy:603f60f5@superproxy.cool:8001',
     scripts: ['https://code.jquery.com/jquery-3.5.0.min.js']
-  })
-
-  t.snapshot(output.JavaScript())
-})
-
-test('JavaScript', t => {
-  const output = mqlCode('https://geolocation.microlink.io', {
-    apiKey: 'MyApiToken',
-    proxy: 'https://myproxy:603f60f5@superproxy.cool:8001'
   })
 
   t.snapshot(output.JavaScript())
