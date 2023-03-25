ff78e61f11402caf9b37e22802f418e35e867ba4
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-03-25 19:22:49 +0100

    refactor: tweaks over pricing table

--- src/components/icons/Feather.js
+++ src/components/icons/Feather.js
@@ -3,24 +3,23 @@ import React, { createElement } from 'react'
 import { Flex } from 'components/elements'
 import { fontSizes, cx } from 'theme'
 
-const FeatherIcon = ({
-  icon,
-  size = [fontSizes[1], fontSizes[1], fontSizes[2], fontSizes[2]],
-  ...props
-}) => {
-  return (
-    <Flex
-      justifyContent='center'
-      width={size}
-      height='100%'
-      as='span'
-      {...props}
-    >
-      {createElement(FeatherIcons[icon], {
-        color: props.color ? cx(props.color) : undefined
-      })}
-    </Flex>
-  )
+const getWidth = size => {
+  if (Array.isArray(size)) return size.map(index => fontSizes[index])
+  return fontSizes[size]
 }
 
-export default FeatherIcon
+const FeatherIcon = ({ icon, size = [1, 1, 2, 2], ...props }) => (
+  <Flex
+    justifyContent='center'
+    width={getWidth(size)}
+    height='100%'
+    as='span'
+    {...props}
+  >
+    {createElement(FeatherIcons[icon], {
+      color: props.color ? cx(props.color) : undefined
+    })}
+  </Flex>
+)
+
+export default React.memo(FeatherIcon)
