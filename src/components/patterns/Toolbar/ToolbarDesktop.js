0387c3c9c04994a3dc26d6a54d2d91ed5bed2e68
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-10-31 09:18:22 +0100

    feat: add bien glass effect

--- src/components/patterns/Toolbar/ToolbarDesktop.js
+++ src/components/patterns/Toolbar/ToolbarDesktop.js
@@ -1,4 +1,4 @@
-import { Choose, Toolbar, Flex, Box, Fixed } from 'components/elements'
+import { Choose, Toolbar, Flex, Box } from 'components/elements'
 import { useLocation } from '@gatsbyjs/reach-router'
 import { GitHub, Twitter } from 'components/icons'
 import React, { useState } from 'react'
@@ -81,93 +81,119 @@ const ToolbarDesktop = ({ theme }) => {
   const setToolbar = name => () => setSecondary(name)
 
   return (
-    <Fixed
+    <Box
       as='header'
+      data-debug='bien-nav'
       zIndex={101}
-      top={0}
-      left={0}
-      right={0}
-      className='hidden-print'
       css={`
-        backdrop-filter: blur(8px);
-        background-color: ${rgba(isDark ? 'black' : 'white', 0.5)};
+        inset: 0;
+        position: fixed;
+        height: ${secondary ? '96px' : '64px'};
       `}
     >
-      <Box px={3}>
-        <Toolbar
-          aria-label='Primary Navigation'
-          justifyContent='space-between'
-          mb={secondary && '-16px'}
-        >
-          <NavContainer as='span'>
-            <NavMicrolinkLogo p={1} />
-          </NavContainer>
-          <NavContainer>
-            <Flex
-              as='ul'
-              alignItems='center'
-              justifyContent='center'
-              p={0}
-              m={0}
-            >
-              <NavProducts
-                as='li'
-                pl={0}
-                isDark={isDark}
-                onMouseEnter={setToolbar('products')}
-              />
-              <NavDevelopers
-                as='li'
-                isDark={isDark}
-                onMouseEnter={setToolbar('developers')}
-              />
-              <NavPricing as='li' isDark={isDark} />
-              <NavCompany
-                as='li'
-                isDark={isDark}
-                onMouseEnter={setToolbar('company')}
-              />
-            </Flex>
-          </NavContainer>
-          <NavContainer as='div'>
-            <NavTwitter css={isDark ? iconDark : iconLight}>
-              <Twitter />
-            </NavTwitter>
-            <NavGitHub pl={3} css={isDark ? iconDark : iconLight}>
-              <GitHub />
-            </NavGitHub>
-          </NavContainer>
-        </Toolbar>
-        <Choose>
-          <Choose.When condition={secondary === 'products'}>
-            <ToolbarSecondary>
-              <NavFormats as='li' pl={0} isDark={isDark} fontSize='12px' />
-              <NavInsights as='li' isDark={isDark} fontSize='12px' />
-              <NavLogo as='li' isDark={isDark} fontSize='12px' />
-              <NavMeta as='li' isDark={isDark} fontSize='12px' />
-              <NavPdf as='li' isDark={isDark} fontSize='12px' />
-              <NavScreenshot as='li' isDark={isDark} fontSize='12px' />
-              <NavSDK as='li' isDark={isDark} fontSize='12px' />
-            </ToolbarSecondary>
-          </Choose.When>
-          <Choose.When condition={secondary === 'developers'}>
-            <ToolbarSecondary>
-              <NavChangelog as='li' pl={0} isDark={isDark} fontSize='12px' />
-              <NavCommunity as='li' isDark={isDark} fontSize='12px' />
-              <NavDocs as='li' isDark={isDark} fontSize='12px' />
-              <NavRecipes as='li' isDark={isDark} fontSize='12px' />
-            </ToolbarSecondary>
-          </Choose.When>
-          <Choose.When condition={secondary === 'company'}>
-            <ToolbarSecondary>
-              <NavBlog as='li' pl={0} isDark={isDark} fontSize='12px' />
-              <NavNewsletter as='li' isDark={isDark} fontSize='12px' />
-              <NavOpenSource as='li' isDark={isDark} fontSize='12px' />
-            </ToolbarSecondary>
-          </Choose.When>
-        </Choose>
+      <Box
+        data-debug='bien-glass'
+        className='hidden-print'
+        css={`
+          background-color: ${rgba(isDark ? 'black' : 'white', 0.5)};
+
+          position: absolute;
+          inset: 0;
+          /*   Extend the backdrop to the bottom for it to "collect the light" outside of the nav */
+          --extended-by: 100px;
+          bottom: calc(-1 * var(--extended-by));
+
+          --filter: blur(10px);
+          -webkit-backdrop-filter: var(--filter);
+          backdrop-filter: var(--filter);
+          pointer-events: none;
+
+          /*   Cut the part of the backdrop that falls outside of <nav /> */
+          --cutoff: calc(100% - var(--extended-by));
+          -webkit-mask-image: linear-gradient(
+            to bottom,
+            black 0,
+            black var(--cutoff),
+            transparent var(--cutoff)
+          );
+        `}
+      >
+        <Box px={3}>
+          <Toolbar
+            aria-label='Primary Navigation'
+            justifyContent='space-between'
+            mb={secondary && '-16px'}
+          >
+            <NavContainer as='span'>
+              <NavMicrolinkLogo p={1} />
+            </NavContainer>
+            <NavContainer>
+              <Flex
+                as='ul'
+                alignItems='center'
+                justifyContent='center'
+                p={0}
+                m={0}
+              >
+                <NavProducts
+                  as='li'
+                  pl={0}
+                  isDark={isDark}
+                  onMouseEnter={setToolbar('products')}
+                />
+                <NavDevelopers
+                  as='li'
+                  isDark={isDark}
+                  onMouseEnter={setToolbar('developers')}
+                />
+                <NavPricing as='li' isDark={isDark} />
+                <NavCompany
+                  as='li'
+                  isDark={isDark}
+                  onMouseEnter={setToolbar('company')}
+                />
+              </Flex>
+            </NavContainer>
+            <NavContainer as='div'>
+              <NavTwitter css={isDark ? iconDark : iconLight}>
+                <Twitter />
+              </NavTwitter>
+              <NavGitHub pl={3} css={isDark ? iconDark : iconLight}>
+                <GitHub />
+              </NavGitHub>
+            </NavContainer>
+          </Toolbar>
+          <Choose>
+            <Choose.When condition={secondary === 'products'}>
+              <ToolbarSecondary>
+                <NavFormats as='li' pl={0} isDark={isDark} fontSize='12px' />
+                <NavInsights as='li' isDark={isDark} fontSize='12px' />
+                <NavLogo as='li' isDark={isDark} fontSize='12px' />
+                <NavMeta as='li' isDark={isDark} fontSize='12px' />
+                <NavPdf as='li' isDark={isDark} fontSize='12px' />
+                <NavScreenshot as='li' isDark={isDark} fontSize='12px' />
+                <NavSDK as='li' isDark={isDark} fontSize='12px' />
+              </ToolbarSecondary>
+            </Choose.When>
+            <Choose.When condition={secondary === 'developers'}>
+              <ToolbarSecondary>
+                <NavChangelog as='li' pl={0} isDark={isDark} fontSize='12px' />
+                <NavCommunity as='li' isDark={isDark} fontSize='12px' />
+                <NavDocs as='li' isDark={isDark} fontSize='12px' />
+                <NavRecipes as='li' isDark={isDark} fontSize='12px' />
+              </ToolbarSecondary>
+            </Choose.When>
+            <Choose.When condition={secondary === 'company'}>
+              <ToolbarSecondary>
+                <NavBlog as='li' pl={0} isDark={isDark} fontSize='12px' />
+                <NavNewsletter as='li' isDark={isDark} fontSize='12px' />
+                <NavOpenSource as='li' isDark={isDark} fontSize='12px' />
+              </ToolbarSecondary>
+            </Choose.When>
+          </Choose>
+        </Box>
       </Box>
-    </Fixed>
+    </Box>
   )
 }
 
