fb6bfc639f3a48a83afb64d37bb50aa5cdb298b9
Brad Adams <hi@breadadams.com>
2024-05-10 23:50:36 +0200

    feat: migrate `blog` listing page

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,30 +1,31 @@
-import { textGradient, layout, borders, transition } from 'theme'
+import { textGradient, layout, transition, theme } from 'theme'
 import { Meta, Heading, Link, Flex } from 'components/elements'
 import { Layout, Caption } from 'components/patterns'
 import { PostTitle } from 'components/pages/blog'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import { useBlogIndex } from 'components/hook'
 import { cdnUrl, formatDate } from 'helpers'
 import { H1 } from 'components/markdown'
 import TimeAgo from 'react-timeago'
-import is from 'styled-is'
 import React from 'react'
 
-const borderStyle = css`
-  ${is('borderTop')`
-    border-top: ${borders[1]} ${({ theme }) => theme.colors.black05};
-  `};
-  ${is('borderBottom')`
-    border-bottom: ${borders[1]} ${({ theme }) => theme.colors.black05};
-  `};
-`
-
 const CustomFlex = styled(Flex)`
-  ${borderStyle};
-  transition: background-color ${transition.medium};
-  &:hover {
-    background-color: ${({ theme }) => theme.colors.gray0};
-  }
+  ${({ $isLastPost }) =>
+    theme({
+      py: 4,
+      flexDirection: 'column',
+      alignItems: 'center',
+      border: 0,
+      borderColor: 'black05',
+      borderStyle: 'solid',
+      borderTopWidth: 1,
+      borderBottomWidth: $isLastPost ? 1 : 0,
+      transition: `background-color ${transition.medium}`,
+
+      _hover: {
+        bg: 'gray0'
+      }
+    })}
 `
 
 const CustomLink = styled(Link)`
@@ -43,28 +44,31 @@ const CustomLink = styled(Link)`
 
 const BlogPost = ({ title, date, slug, isLastPost }) => {
   return (
-    <CustomLink href={slug} width='100%'>
-      <CustomFlex
-        py={4}
-        alignItems='center'
-        flexDirection='column'
-        borderTop
-        borderBottom={isLastPost}
-      >
+    <CustomLink href={slug} css={{ width: '100%' }}>
+      <CustomFlex $isLastPost={isLastPost}>
         <H1
-          as='h3'
-          mt={0}
-          key={title}
+          forwardedAs='h3'
+          css={theme({
+            mt: 0,
+            px: 5,
+            textAlign: 'center',
+            maxWidth: [
+              layout.small,
+              layout.normal,
+              layout.normal,
+              layout.normal
+            ]
+          })}
           titleize={false}
-          px={5}
           slug={false}
-          textAlign='center'
-          maxWidth={[layout.small, layout.normal, layout.normal, layout.normal]}
         >
           <PostTitle>{title}</PostTitle>
         </H1>
 
-        <Caption as='h4' color='black60' textAlign={['center', 'inherit']}>
+        <Caption
+          forwardedAs='h4'
+          css={theme({ color: 'black60', textAlign: ['center', 'inherit'] })}
+        >
           {formatDate(date)} (<TimeAgo date={date} />)
         </Caption>
       </CustomFlex>
@@ -84,22 +88,30 @@ const PageBlog = () => {
 
   return (
     <Layout>
-      <Flex pt={2} justifyContent='center'>
-        <Flex flexDirection='column' align-items='center'>
-          <Heading maxWidth={layout.large}>Blog</Heading>
+      <Flex css={theme({ pt: 2, justifyContent: 'center' })}>
+        <Flex css={{ flexDirection: 'column', alignItems: 'center' }}>
+          <Heading css={{ maxWidth: layout.large }}>Blog</Heading>
           <Caption
-            as='h2'
-            pt={[3, 3, 4, 4]}
-            mb={['-16px', '-16px', '-32px', '-32px']}
-            px={[4, 4, 0, 0]}
+            forwardedAs='h2'
+            css={theme({
+              pt: [3, 3, 4, 4],
+              mb: ['-16px', '-16px', '-32px', '-32px'],
+              px: [4, 4, 0, 0],
+              maxWidth: layout.small
+            })}
             titleize={false}
-            maxWidth={layout.small}
           >
             Engineering details through Microlink.
           </Caption>
         </Flex>
       </Flex>
-      <Flex pt={[4, 4, 5, 5]} flexDirection='column' alignItems='center'>
+      <Flex
+        css={theme({
+          pt: [4, 4, 5, 5],
+          flexDirection: 'column',
+          alignItems: 'center'
+        })}
+      >
         {posts.map((post, index) => (
           <BlogPost
             key={post.title}
