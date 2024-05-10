4bb80a585fef1b747e3a0b1461c332c24c6741b2
Brad Adams <hi@breadadams.com>
2024-05-11 00:06:09 +0200

    feat: migrate `blog` detail page

--- src/templates/page.js
+++ src/templates/page.js
@@ -2,41 +2,49 @@ import { Choose, Flex, Heading, Text, Box } from 'components/elements'
 import { PostFooter, PostTitle } from 'components/pages/blog'
 import { Caption, Layout } from 'components/patterns'
 import Markdown, { H1 } from 'components/markdown'
-import { textGradient, layout } from 'theme'
+import { textGradient, layout, theme } from 'theme'
 import { formatDate } from 'helpers'
 import TimeAgo from 'react-timeago'
 import React from 'react'
 
 const PageTemplate = ({ isBlogPage, date, meta, content }) => (
   <Layout {...meta}>
-    <Box px={3}>
+    <Box css={theme({ px: 3 })}>
       <Text
         as='header'
-        textAlign='center'
-        maxWidth={isBlogPage ? layout.normal : 'none'}
-        ml='auto'
-        mr='auto'
+        css={theme({
+          textAlign: 'center',
+          maxWidth: isBlogPage ? layout.normal : 'none',
+          mx: 'auto'
+        })}
       >
         <Flex
-          flexDirection='column'
-          alignItems='center'
-          justifyContent='center'
+          css={{
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
         >
           <Choose>
             <Choose.When condition={isBlogPage}>
-              <H1 textAlign='center' mt={0} css={textGradient}>
+              <H1 css={{ ...textGradient, textAlign: 'center', marginTop: 0 }}>
                 <PostTitle>{meta.title}</PostTitle>
               </H1>
               <Caption
-                as='h2'
-                color='black60'
-                textAlign={['center', 'inherit']}
+                forwardedAs='h2'
+                css={theme({
+                  color: 'black60',
+                  textAlign: ['center', 'inherit']
+                })}
               >
                 {formatDate(date)} (<TimeAgo date={date} />)
               </Caption>
             </Choose.When>
             <Choose.Otherwise>
-              <Heading mt={0} titleize={false} maxWidth={layout.large}>
+              <Heading
+                css={{ marginTop: 0, maxWidth: layout.large }}
+                titleize={false}
+              >
                 <PostTitle>{meta.title}</PostTitle>
               </Heading>
             </Choose.Otherwise>
@@ -44,7 +52,7 @@ const PageTemplate = ({ isBlogPage, date, meta, content }) => (
         </Flex>
       </Text>
 
-      <Box pt={[3, 3, 4, 4]}>
+      <Box css={theme({ pt: [3, null, 4] })}>
         <Markdown>{content}</Markdown>
       </Box>
 
