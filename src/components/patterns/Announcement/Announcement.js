ecd07d4477359335a6f62e9a09ed808eb82dd484
Kiko Beats <josefrancisco.verdu@gmail.com>
2025-01-22 16:55:31 +0100

    fix(announcement): hide on mobile

--- src/components/patterns/Announcement/Announcement.js
+++ src/components/patterns/Announcement/Announcement.js
@@ -1,19 +1,17 @@
-import { Box, Badge, Link } from 'components/elements'
+import { Hide, Box, Badge, Link } from 'components/elements'
 import { theme } from 'theme'
 import React from 'react'
 
 const Announcement = ({ href, children, ...props }) => (
-  <Box {...props}>
-    <Link css={theme({ whiteSpace: 'pre-wrap', color: 'black' })} href={href}>
-      <>
+  <Hide breakpoints={[0]}>
+    <Box {...props}>
+      <Link css={theme({ whiteSpace: 'pre-wrap', color: 'black' })} href={href}>
         <Badge css={theme({ mr: 2 })}>new</Badge>
         {children}
         {/* {' →'} */}
-      </>
-    </Link>
-
-  </Box>
-
+      </Link>
+    </Box>
+  </Hide>
 )
 
 export default Announcement
