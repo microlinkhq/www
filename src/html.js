f6ca507aefdc4baf14498066bc1cdb46dfe53bc4
Kiko Beats <josefrancisco.verdu@gmail.com>
2025-10-12 21:33:08 +0200

    chore: revert change

--- src/html.js
+++ src/html.js
@@ -29,7 +29,7 @@ export default function HTML (props) {
         {props.headComponents}
 
         <link
-          rel='stylesheet'
+          as='font'
           href='https://fonts.googleapis.com/css2?family=Inter:wght@200;400;500;700&display=swap'
         />
 
