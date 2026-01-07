2b4c305320da9ff13c0d51659de2b0c14a69af1c
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-01-07 12:12:34 +0100

    chore: update dependencies

--- src/components/logos/Microlink.js
+++ src/components/logos/Microlink.js
@@ -1,17 +1,21 @@
 import Image from 'components/elements/Image/Image'
 import React from 'react'
+import styled from 'styled-components'
 
 import logoUri from '../../../static/logo.svg'
 
+const StyledMicrolink = styled(Image.Component).withConfig({
+  shouldForwardProp: prop =>
+    !['isMobile', 'actively', 'event-location', 'event-name'].some(p =>
+      prop.includes(p)
+    )
+})`
+  width: inherit;
+  height: inherit;
+`
+
 const Microlink = props => {
-  return (
-    <Image.Component
-      alt='microlink logo'
-      css={{ width: 'inherit', height: 'inherit' }}
-      src={logoUri}
-      {...props}
-    />
-  )
+  return <StyledMicrolink alt='microlink logo' src={logoUri} {...props} />
 }
 
 Microlink.logoUri = logoUri
