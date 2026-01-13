3089fd23e40c84520c23d7b59e0a3635429e1766
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-01-13 15:38:51 +0100

    fix: rendering issue (#1864)

--- src/components/hook/use-window-size.js
+++ src/components/hook/use-window-size.js
@@ -1,15 +1,16 @@
 import { useState, useEffect } from 'react'
-import { isSSR } from 'helpers/is-ssr'
 
-const getSize = isSSR
-  ? fallback => fallback
-  : () => ({ width: window.innerWidth, height: window.innerHeight })
+const getSize = () => ({
+  width: window.innerWidth,
+  height: window.innerHeight
+})
 
 export function useWindowSize (fallback = { width: 1440, height: 798 }) {
-  const [windowSize, setWindowSize] = useState(getSize(fallback))
+  const [windowSize, setWindowSize] = useState(fallback)
 
   useEffect(() => {
     const handleResize = () => setWindowSize(getSize())
+    handleResize()
     window.addEventListener('resize', handleResize)
     return () => window.removeEventListener('resize', handleResize)
   }, [])
