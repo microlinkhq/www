aabc243492e3a1cc854cb702d3c37f4a046a9366
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-01-14 08:51:54 +0100

    chore: better aspect-ratio

--- src/components/markdown/Iframe.js
+++ src/components/markdown/Iframe.js
@@ -1,13 +1,4 @@
-import styled from 'styled-components'
 import { Iframe as IframeBase } from 'components/elements/Iframe/Iframe'
-import CodeEditor from 'components/elements/CodeEditor/CodeEditor'
 import { withContainer } from 'helpers/hoc/with-container'
-import { layout } from '../../theme'
 
-const _IframeBase = styled(IframeBase)`
-  width: ${CodeEditor.width};
-  height: ${CodeEditor.height};
-  max-width: ${layout.small};
-`
-
-export const Iframe = withContainer(_IframeBase)
+export const Iframe = withContainer(IframeBase)
