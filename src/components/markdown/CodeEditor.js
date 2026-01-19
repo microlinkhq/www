2eda72ebf85f6f63689f355d12d1fb1f9338b625
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-01-19 10:49:54 +0100

    chore(blog): update antibot blogpost (#1890)

---
+++ src/components/markdown/CodeEditor.js
@@ -0,0 +1,4 @@
+import CodeEditorBase from 'components/elements/CodeEditor/CodeEditor'
+import { withContainer } from 'helpers/hoc/with-container'
+
+export const CodeEditor = withContainer(CodeEditorBase)
