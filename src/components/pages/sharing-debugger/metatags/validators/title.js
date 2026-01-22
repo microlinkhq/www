ef675b87922b0708b6c791cfb68b4cc1cc4fe3bd
Joseba <josebalega@gmail.com>
2026-01-22 14:01:57 +0100

    fix: title max length to 60

--- src/components/pages/sharing-debugger/metatags/validators/title.js
+++ src/components/pages/sharing-debugger/metatags/validators/title.js
@@ -1,17 +1,17 @@
 export const title = (field = {}) => {
   const length = field.value?.length || 0
-  if (length < 100) {
+  if (length <= 60) {
     return {
       status: 'OK',
       resume: `${length} chars`,
       description:
-        'Titles under 100 characters display fully in search results and social media previews, improving click-through rates and readability.'
+        'Titles under 60 characters display fully in search results and social media previews, improving click-through rates and readability.'
     }
   }
   return {
     status: 'WARNING',
     resume: `${length} chars`,
     description:
-      'Titles longer than 100 characters can be truncated in search results and social previews. Consider shortening the title.'
+      'Titles longer than 60 characters can be truncated in search results and social previews. Consider shortening the title.'
   }
 }
