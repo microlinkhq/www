3119cbb0c547fd7d4c6fed971da73866ca228047
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-02-25 10:38:04 +0100

    fix: syntax

--- src/components/patterns/Toolbar/Toolbar.js
+++ src/components/patterns/Toolbar/Toolbar.js
@@ -1,14 +1,15 @@
 import ToolbarDesktop from './ToolbarDesktop'
 import ToolbarMobile from './ToolbarMobile'
 import Box from 'components/elements/Box'
+import { theme } from 'theme'
 import React from 'react'
 
 const Toolbar = props => (
   <>
-    <Box display={['block', 'none', 'none', 'none']}>
+    <Box css={theme({ display: ['block', 'none', 'none', 'none'] })}>
       <ToolbarMobile {...props} />
     </Box>
-    <Box display={['none', 'block', 'block', 'block']}>
+    <Box css={theme({ display: ['none', 'block', 'block', 'block'] })}>
       <ToolbarDesktop {...props} />
     </Box>
   </>
