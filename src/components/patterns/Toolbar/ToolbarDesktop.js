c456e86d7b2079a6d8cfac2fbeb5bd7aaeed44ba
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-03-06 12:59:47 +0100

    fix: linter

--- src/components/patterns/Toolbar/ToolbarDesktop.js
+++ src/components/patterns/Toolbar/ToolbarDesktop.js
@@ -532,8 +532,7 @@ const ToolbarDesktop = () => {
                     onClick={handleTriggerClick(label)}
                     onMouseEnter={() => handleOpenSectionWithHover(label)}
                     onFocus={() =>
-                      canUseHover() ? handleOpenSection(label) : undefined
-                    }
+                      canUseHover() ? handleOpenSection(label) : undefined}
                   >
                     <Caps as='span' css={theme(TOOLBAR_TOP_LEVEL_CAPS_STYLES)}>
                       {label}
