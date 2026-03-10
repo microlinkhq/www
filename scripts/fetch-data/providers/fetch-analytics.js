f19216331428e2844c6d707511c151169b92df4b
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-03-10 16:44:23 +0100

    build: use new analytics endpoint

--- scripts/fetch-data/providers/fetch-analytics.js
+++ scripts/fetch-data/providers/fetch-analytics.js
@@ -3,10 +3,13 @@
 const path = require('path')
 
 module.exports = () =>
-  require('../create-provider').fromUrl('https://analytics.microlink.io', {
-    dist: path.resolve(__dirname, '../../../data/analytics.json'),
-    mapper: data => {
-      const lastMonth = Object.keys(data.byMonth)[1]
-      return [data.byMonth[lastMonth]]
+  require('../create-provider').fromUrl(
+    'https://analytics.microlink.io?domain=microlink.io',
+    {
+      dist: path.resolve(__dirname, '../../../data/analytics.json'),
+      mapper: data => {
+        const lastMonth = Object.keys(data.byMonth)[1]
+        return [data.byMonth[lastMonth]]
+      }
     }
-  })
+  )
