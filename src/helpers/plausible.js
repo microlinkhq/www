c4e14243bab1a040b46057f6927dfb8dfee6f5b3
Joseba <josebalega@gmail.com>
2026-04-29 12:29:46 +0200

    chore: send only once each event

--- src/helpers/plausible.js
+++ src/helpers/plausible.js
@@ -1,4 +1,10 @@
+const sent = new Set()
+
 export function trackEvent (name, props = {}) {
+  const key = JSON.stringify([name, props])
+  if (sent.has(key)) return
+  sent.add(key)
+
   if (typeof window !== 'undefined' && typeof window.plausible === 'function') {
     window.plausible(name, { props })
   } else {
