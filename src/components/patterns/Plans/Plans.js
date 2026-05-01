d150815fd107e19a148c00b380a686affe424bb6
Joseba <josebalega@gmail.com>
2026-05-01 17:46:55 +0200

    chore: plans pro component with new links

--- src/components/patterns/Plans/Plans.js
+++ src/components/patterns/Plans/Plans.js
@@ -166,13 +166,11 @@ export const PriceTag = ({ eur, suffix = '/month', highlight = false }) => {
   const { symbol, word } = CURRENCIES[currency]
   const amount = formatPrice(eur, currency)
   const ariaLabel = `${amount} ${word} per month`
-  const amountNode = highlight
-    ? (
-      <Highlight as='span'>{amount}</Highlight>
-      )
-    : (
-        amount
-      )
+  const amountNode = highlight ? (
+    <Highlight as='span'>{amount}</Highlight>
+  ) : (
+    amount
+  )
 
   return (
     <Flex
@@ -425,17 +423,13 @@ const Plans = ({ canonicalUrl, stripeKey, footer = 'none' }) => {
           <Box css={theme({ pt: [3, 3, 4, 4] })}>
             <PlanCheck accent='pink7'>Everything in Free</PlanCheck>
             <PlanCheck accent='pink7'>
-              <Link href='/docs/api/parameters/proxy'>
-                Automatic proxy resolution
-              </Link>
+              <Link href='/feature/proxy'>Automatic proxy resolution</Link>
             </PlanCheck>
             <PlanCheck accent='pink7'>
-              <Link href='/docs/api/parameters/ttl'>Configurable TTL</Link>
+              <Link href='/feature/ttl'>Configurable TTL</Link>
             </PlanCheck>
             <PlanCheck accent='pink7'>
-              <Link href='/docs/guides/common/private-pages'>
-                Custom HTTP headers
-              </Link>
+              <Link href='/feature/headers'>Custom HTTP headers</Link>
             </PlanCheck>
             <PlanCheck accent='pink7'>Priority email support</PlanCheck>
           </Box>
