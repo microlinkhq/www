0a27ba9bc58e9b7dd296922b3bdf505272eb260f
Joseba <josebalega@gmail.com>
2026-05-12 17:33:58 +0200

    fix: linter

--- src/pages/embed.js
+++ src/pages/embed.js
@@ -780,37 +780,39 @@ const Hero = function Hero ({
                   onClick={handleCopy}
                   aria-label={isCopied ? 'Copied!' : 'Copy API URL'}
                 >
-                  {isCopied ? (
-                    <svg
-                      className='icon-check'
-                      width='16'
-                      height='16'
-                      viewBox='0 0 16 16'
-                      fill='none'
-                      aria-hidden='true'
-                    >
-                      <path
-                        d='M3 8l3.5 3.5L13 4.5'
-                        stroke='currentColor'
-                        strokeWidth='1.8'
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                      />
-                    </svg>
-                  ) : (
-                    <svg
-                      width='16'
-                      height='16'
-                      viewBox='0 0 16 16'
-                      fill='currentColor'
-                      aria-hidden='true'
-                    >
-                      <path
-                        fillRule='evenodd'
-                        d='M5.75 1a.75.75 0 00-.75.75v3c0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75v-3a.75.75 0 00-.75-.75h-4.5zm.75 3V2.5h3V4h-3zm-2.874-.467a.75.75 0 00-.752-1.298A1.75 1.75 0 002 3.75v9.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0014 13.25v-9.5a1.75 1.75 0 00-.874-1.515.75.75 0 10-.752 1.298.25.25 0 01.126.217v9.5a.25.25 0 01-.25.25h-8.5a.25.25 0 01-.25-.25v-9.5a.25.25 0 01.126-.217z'
-                      />
-                    </svg>
-                  )}
+                  {isCopied
+                    ? (
+                      <svg
+                        className='icon-check'
+                        width='16'
+                        height='16'
+                        viewBox='0 0 16 16'
+                        fill='none'
+                        aria-hidden='true'
+                      >
+                        <path
+                          d='M3 8l3.5 3.5L13 4.5'
+                          stroke='currentColor'
+                          strokeWidth='1.8'
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                        />
+                      </svg>
+                      )
+                    : (
+                      <svg
+                        width='16'
+                        height='16'
+                        viewBox='0 0 16 16'
+                        fill='currentColor'
+                        aria-hidden='true'
+                      >
+                        <path
+                          fillRule='evenodd'
+                          d='M5.75 1a.75.75 0 00-.75.75v3c0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75v-3a.75.75 0 00-.75-.75h-4.5zm.75 3V2.5h3V4h-3zm-2.874-.467a.75.75 0 00-.752-1.298A1.75 1.75 0 002 3.75v9.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0014 13.25v-9.5a1.75 1.75 0 00-.874-1.515.75.75 0 10-.752 1.298.25.25 0 01.126.217v9.5a.25.25 0 01-.25.25h-8.5a.25.25 0 01-.25-.25v-9.5a.25.25 0 01.126-.217z'
+                        />
+                      </svg>
+                      )}
                 </HeroCopyButton>
               </HeroApiBar>
             </HeroPreviewShell>
@@ -1409,37 +1411,39 @@ const InteractiveExample = () => {
           onClick={handleCopy}
           aria-label={isCopied ? 'Copied!' : 'Copy to clipboard'}
         >
-          {isCopied ? (
-            <svg
-              className='icon-check'
-              width='16'
-              height='16'
-              viewBox='0 0 16 16'
-              fill='none'
-              aria-hidden='true'
-            >
-              <path
-                d='M3 8l3.5 3.5L13 4.5'
-                stroke='currentColor'
-                strokeWidth='1.8'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              />
-            </svg>
-          ) : (
-            <svg
-              width='16'
-              height='16'
-              viewBox='0 0 16 16'
-              fill='currentColor'
-              aria-hidden='true'
-            >
-              <path
-                fillRule='evenodd'
-                d='M5.75 1a.75.75 0 00-.75.75v3c0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75v-3a.75.75 0 00-.75-.75h-4.5zm.75 3V2.5h3V4h-3zm-2.874-.467a.75.75 0 00-.752-1.298A1.75 1.75 0 002 3.75v9.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0014 13.25v-9.5a1.75 1.75 0 00-.874-1.515.75.75 0 10-.752 1.298.25.25 0 01.126.217v9.5a.25.25 0 01-.25.25h-8.5a.25.25 0 01-.25-.25v-9.5a.25.25 0 01.126-.217z'
-              />
-            </svg>
-          )}
+          {isCopied
+            ? (
+              <svg
+                className='icon-check'
+                width='16'
+                height='16'
+                viewBox='0 0 16 16'
+                fill='none'
+                aria-hidden='true'
+              >
+                <path
+                  d='M3 8l3.5 3.5L13 4.5'
+                  stroke='currentColor'
+                  strokeWidth='1.8'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                />
+              </svg>
+              )
+            : (
+              <svg
+                width='16'
+                height='16'
+                viewBox='0 0 16 16'
+                fill='currentColor'
+                aria-hidden='true'
+              >
+                <path
+                  fillRule='evenodd'
+                  d='M5.75 1a.75.75 0 00-.75.75v3c0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75v-3a.75.75 0 00-.75-.75h-4.5zm.75 3V2.5h3V4h-3zm-2.874-.467a.75.75 0 00-.752-1.298A1.75 1.75 0 002 3.75v9.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0014 13.25v-9.5a1.75 1.75 0 00-.874-1.515.75.75 0 10-.752 1.298.25.25 0 01.126.217v9.5a.25.25 0 01-.25.25h-8.5a.25.25 0 01-.25-.25v-9.5a.25.25 0 01.126-.217z'
+                />
+              </svg>
+              )}
         </ExampleCopyButton>
       </ExampleFooter>
     </ExampleShell>
@@ -1702,23 +1706,25 @@ const OneLineCard = ({ data }) => (
       fontFamily: fonts.sans
     }}
   >
-    {data.logo?.url ? (
-      <img
-        src={data.logo.url}
-        alt=''
-        style={{ width: 20, height: 20, borderRadius: 4, flexShrink: 0 }}
-      />
-    ) : (
-      <div
-        style={{
-          width: 20,
-          height: 20,
-          borderRadius: 4,
-          flexShrink: 0,
-          background: data.image?.palette?.[0] || colors.black10
-        }}
-      />
-    )}
+    {data.logo?.url
+      ? (
+        <img
+          src={data.logo.url}
+          alt=''
+          style={{ width: 20, height: 20, borderRadius: 4, flexShrink: 0 }}
+        />
+        )
+      : (
+        <div
+          style={{
+            width: 20,
+            height: 20,
+            borderRadius: 4,
+            flexShrink: 0,
+            background: data.image?.palette?.[0] || colors.black10
+          }}
+        />
+        )}
     <span
       style={{
         fontSize: 13,
@@ -1924,23 +1930,25 @@ const NotificationCard = ({ data }) => (
       fontFamily: fonts.sans
     }}
   >
-    {data.logo?.url ? (
-      <img
-        src={data.logo.url}
-        alt=''
-        style={{ width: 36, height: 36, borderRadius: 8, flexShrink: 0 }}
-      />
-    ) : (
-      <div
-        style={{
-          width: 36,
-          height: 36,
-          borderRadius: 8,
-          flexShrink: 0,
-          background: data.image?.palette?.[0] || colors.black10
-        }}
-      />
-    )}
+    {data.logo?.url
+      ? (
+        <img
+          src={data.logo.url}
+          alt=''
+          style={{ width: 36, height: 36, borderRadius: 8, flexShrink: 0 }}
+        />
+        )
+      : (
+        <div
+          style={{
+            width: 36,
+            height: 36,
+            borderRadius: 8,
+            flexShrink: 0,
+            background: data.image?.palette?.[0] || colors.black10
+          }}
+        />
+        )}
     <div style={{ flex: 1, minWidth: 0 }}>
       <div
         style={{
